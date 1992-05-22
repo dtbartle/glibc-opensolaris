ca73a8bca0877fc684322e1c328edf43ac78ca9b
roland <roland>
1992-05-22 07:51:53 +0000

    entered into RCS

--- stdlib/alloca.h
+++ stdlib/alloca.h
@@ -13,7 +13,7 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with the GNU C Library; see the file COPYING.LIB.  If
-not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+not, write to the, 1992 Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 #ifndef	_ALLOCA_H
@@ -23,18 +23,22 @@ Cambridge, MA 02139, USA.  */
 #define	__need_size_t
 #include <stddef.h>
 
+__BEGIN_DECLS
+
 /* Remove any previous definitions.  */
 #undef	__alloca
 #undef	alloca
 
 /* Allocate a block that will be freed when the calling function exits.  */
-extern PTR EXFUN(__alloca, (size_t __size));
-extern PTR EXFUN(alloca, (size_t __size));
+extern __ptr_t __alloca __P ((size_t __size));
+extern __ptr_t alloca __P ((size_t __size));
 
 #ifdef	__GNUC__
 #define	__alloca(size)	__builtin_alloca(size)
-#endif	/* GCC.  */
+#endif /* GCC.  */
 
 #define	alloca(size)	__alloca(size)
 
-#endif	/* alloca.h */
+__END_DECLS
+
+#endif /* alloca.h */
