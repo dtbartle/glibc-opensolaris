24bc962ffdd841304c8797bad33e719709287e16
roland <roland>
1995-01-17 22:31:56 +0000

    (_hurd_sig_post): __sig_post renamed to __msg_sig_post.

--- hurd/hurdkill.c
+++ hurd/hurdkill.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -42,7 +42,7 @@ _hurd_sig_post (pid_t pid, int sig, mach_port_t arg_refport)
 			      ((refport = arg_refport), 0),
 			      /* If no message port we cannot send signals.  */
 			      msgport == MACH_PORT_NULL ? EPERM :
-			      __sig_post (msgport, sig, refport));
+			      __msg_sig_post (msgport, sig, refport));
       if (! err)
 	delivered = 1;
     }
