ba581f08709f83f0f9fd94d906b558ec2e5fc4d0
roland <roland>
1995-01-21 14:54:43 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/link.c
+++ sysdeps/mach/hurd/link.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -56,3 +56,5 @@ DEFUN(__link, (from, to), CONST char *from AND CONST char *to)
     return __hurd_fail (err);
   return 0;
 }
+
+weak_alias (__link, link)
