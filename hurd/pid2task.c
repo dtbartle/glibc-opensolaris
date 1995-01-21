38f158e44856dee40521fd43874157fad38f73df
roland <roland>
1995-01-21 15:02:13 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- hurd/pid2task.c
+++ hurd/pid2task.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -28,3 +28,5 @@ __pid2task (pid_t pid)
 
   return err ? (__hurd_fail (err), MACH_PORT_NULL) : task;
 }
+
+weak_alias (__pid2task, pid2task)
