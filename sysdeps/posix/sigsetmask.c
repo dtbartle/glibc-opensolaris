ceeba942f1b09461dd057ef0e4770724b0eb85ae
roland <roland>
1995-01-21 15:39:25 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/posix/sigsetmask.c
+++ sysdeps/posix/sigsetmask.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -51,3 +51,5 @@ DEFUN(__sigsetmask, (mask), int mask)
 
   return mask;
 }
+
+weak_alias (__sigsetmask, sigsetmask)
--- sysdeps/stub/sigsetmask.c
+++ sysdeps/stub/sigsetmask.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,12 +26,6 @@ DEFUN(__sigsetmask, (mask), int mask)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (sigsetmask)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__sigsetmask);
-
-#endif	/* GNU stabs.  */
+weak_alias (__sigsetmask, sigsetmask)
