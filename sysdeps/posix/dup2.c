f0b408f9d0ce50c3cee82d872fd42b02bce97990
roland <roland>
1995-01-21 14:30:16 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/posix/dup2.c
+++ sysdeps/posix/dup2.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -55,3 +55,5 @@ DEFUN(__dup2, (fd, fd2), int fd AND int fd2)
 
   return fcntl (fd, F_DUPFD, fd2);
 }
+
+weak_alias (__dup2, dup2)
