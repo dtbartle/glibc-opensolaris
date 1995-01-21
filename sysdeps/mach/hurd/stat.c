f56167e608070881e6c116874b28c4d942c7f342
roland <roland>
1995-01-21 15:23:02 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/stat.c
+++ sysdeps/mach/hurd/stat.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -36,3 +36,5 @@ DEFUN(__stat, (file, buf), CONST char *file AND struct stat *buf)
     return __hurd_fail (err);
   return 0;
 }
+
+weak_alias (__stat, stat)
