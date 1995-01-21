b5273d0e6bc1355d066533f063e71dd3458fa7ec
roland <roland>
1995-01-21 15:08:52 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/setgid.c
+++ sysdeps/stub/setgid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,12 +31,6 @@ DEFUN(__setgid, (gid), gid_t gid)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (setgid)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__setgid);
-
-#endif	/* GNU stabs.  */
+weak_alias (__setgid, setgid)
