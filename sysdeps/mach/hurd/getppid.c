0c04a8c23e6585f6fab5a759167b51c48dd6a6fd
roland <roland>
1995-01-21 14:48:45 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/getppid.c
+++ sysdeps/mach/hurd/getppid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -30,3 +30,5 @@ DEFUN_VOID(__getppid)
   /* Assumes atomic word fetch and store, so doesn't lock _hurd_pid_lock.  */
   return _hurd_ppid;
 }
+
+weak_alias (__getppid, getppid)
