91402425c2d39b1ea23c94ec49309ced2c277af0
roland <roland>
1995-01-21 14:39:17 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/unix/getdents.c
+++ sysdeps/unix/getdents.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,3 +31,6 @@ DEFUN(__getdirentries, (fd, buf, nbytes, basep),
 
   return __read (fd, buf, nbytes);
 }
+
+weak_alias (__getdirentries, getdirentries)
+
