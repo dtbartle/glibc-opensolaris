36b947538f909eb2526032e3a6644f1813c6ea26
roland <roland>
1995-01-21 14:46:18 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/gethostname.c
+++ sysdeps/mach/hurd/gethostname.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -44,3 +44,5 @@ DEFUN(__gethostname, (name, len),
     return __hurd_fail (ENAMETOOLONG);
   return 0;
 }
+
+weak_alias (__gethostname, gethostname)
