96c1eb65ce0752eb4846cb9f7d3556f7910ad77a
roland <roland>
1995-01-21 14:29:31 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/drem.c
+++ sysdeps/stub/drem.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -28,3 +28,6 @@ DEFUN(__drem, (x, y),
   errno = ENOSYS;
   return 0.0;
 }
+
+stub_warning (drem)
+weak_alias (__drem, drem)
