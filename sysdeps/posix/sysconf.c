2375963cbe267fbe42e03733aadfe799829c5d93
roland <roland>
1995-01-21 15:25:12 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/posix/sysconf.c
+++ sysdeps/posix/sysconf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -179,3 +179,5 @@ DEFUN(__sysconf, (name), int name)
 #endif
     }
 }
+
+weak_alias (__sysconf, sysconf)
