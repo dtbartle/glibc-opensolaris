7530c0a843aa7baf1641c34ee9b16ab6956e2603
roland <roland>
1995-01-21 15:17:25 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/sigpause.c
+++ sysdeps/stub/sigpause.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,12 +26,6 @@ DEFUN(__sigpause, (mask), int mask)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (sigpause)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__sigpause);
-
-#endif	/* GNU stabs.  */
+weak_alias (__sigpause, sigpause)
