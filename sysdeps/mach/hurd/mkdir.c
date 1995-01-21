80ed4463a9837eae4c98a54622a1c098745623d7
roland <roland>
1995-01-21 14:59:05 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/mkdir.c
+++ sysdeps/mach/hurd/mkdir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -37,3 +37,5 @@ DEFUN(__mkdir, (file_name, mode), CONST char *file_name AND mode_t mode)
     return __hurd_fail (err);
   return 0;
 }
+
+weak_alias (__mkdir, mkdir)
--- sysdeps/stub/mknod.c
+++ sysdeps/stub/mknod.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,12 +31,6 @@ DEFUN(__mknod, (path, mode, dev),
   errno = ENOSYS;
   return -1;
 }
+stub_warning (mknod)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__mknod);
-
-#endif	/* GNU stabs.  */
+weak_alias (__mknod, mknod)
