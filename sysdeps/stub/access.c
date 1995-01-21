094c8af518f4def93ea98547fd0b3ba0dfd6eaa7
roland <roland>
1995-01-21 14:17:36 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/access.c
+++ sysdeps/stub/access.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -34,12 +34,6 @@ DEFUN(__access, (file, type), CONST char *file AND int type)
   errno = ENOSYS;
   return(-1);
 }
+stub_warning (access)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__access);
-
-#endif	/* GNU stabs.  */
+weak_alias (__access, access)
