46aa928b0ef9f15dd79c9ec2075ab9cbe3a218f5
roland <roland>
1995-01-21 15:27:22 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/umask.c
+++ sysdeps/stub/umask.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -28,12 +28,6 @@ DEFUN(__umask, (mask), mode_t mask)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (umask)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__umask);
-
-#endif	/* GNU stabs.  */
+weak_alias (__umask, umask)
