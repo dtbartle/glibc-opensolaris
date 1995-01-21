c4580cbb89b8742e0a3428f6434095a7fc31802f
roland <roland>
1995-01-21 14:26:41 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/chdir.c
+++ sysdeps/mach/hurd/chdir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -41,3 +41,5 @@ DEFUN(__chdir, (file_name), CONST char *file_name)
   _hurd_port_set (&_hurd_ports[INIT_PORT_CWDIR], dir);
   return 0;
 }
+
+weak_alias (__chdir, chdir)
--- sysdeps/stub/chmod.c
+++ sysdeps/stub/chmod.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -35,15 +35,6 @@ DEFUN(__chmod, (file, mode), CONST char *file AND mode_t mode)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (chmod)
 
-
-
-
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__chmod);
-
-#endif	/* GNU stabs.  */
+weak_alias (__chmod, chmod)
