0b3f393a3655740f951d69acdf0e35715dc4421d
roland <roland>
1995-01-21 14:55:33 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/logb.c
+++ sysdeps/stub/logb.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -27,3 +27,5 @@ DEFUN(__logb, (x), double x)
   errno = ENOSYS;
   return 0.0;
 }
+stub_warning (logb)
+weak_alias (__logb, logb)
--- sysdeps/stub/lseek.c
+++ sysdeps/stub/lseek.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -44,12 +44,6 @@ DEFUN(__lseek, (fd, offset, whence), int fd AND off_t offset AND int whence)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (lseek)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__lseek);
-
-#endif	/* GNU stabs.  */
+weak_alias (__lseek, lseek)
