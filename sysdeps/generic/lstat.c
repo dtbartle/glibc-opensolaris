2b24f835d57dd2f01cf62868b11a015c8bcaeef6
roland <roland>
1995-01-21 14:55:48 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/generic/lstat.c
+++ sysdeps/generic/lstat.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -25,3 +25,5 @@ Cambridge, MA 02139, USA.  */
 function_alias (__lstat, __stat, int, (file, buf),
 		DEFUN(__lstat, (file, buf),
 		      CONST char *file AND struct stat *buf))
+
+weak_alias (__lstat, lstat)
--- sysdeps/mach/hurd/lseek.c
+++ sysdeps/mach/hurd/lseek.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,3 +31,5 @@ DEFUN(__lseek, (fd, offset, whence), int fd AND off_t offset AND int whence)
     return __hurd_dfail (fd, err);
   return offset;
 }
+
+weak_alias (__lseek, lseek)
