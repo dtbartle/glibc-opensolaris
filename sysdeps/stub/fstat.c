70467d908bbbf7a35badd97f688e4a6637a432b0
roland <roland>
1995-01-21 14:37:16 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/fstat.c
+++ sysdeps/stub/fstat.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -39,12 +39,6 @@ DEFUN(__fstat, (fd, buf), int fd AND struct stat *buf)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (fstat)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__fstat);
-
-#endif	/* GNU stabs.  */
+weak_alias (__fstat, fstat)
