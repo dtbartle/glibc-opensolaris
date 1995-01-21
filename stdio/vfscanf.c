69e45829cec1c9c2d71fa63a630e0e19cb6944a4
roland <roland>
1995-01-21 15:30:45 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- stdio/vfscanf.c
+++ stdio/vfscanf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -560,3 +560,5 @@ DEFUN(__vfscanf, (s, format, arg),
 
   conv_error();
 }
+
+weak_alias (__vfscanf, vfscanf)
