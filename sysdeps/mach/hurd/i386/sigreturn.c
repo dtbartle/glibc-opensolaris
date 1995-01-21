2ab73d211d594407cddfe0ea9f6448d42a6dcafc
roland <roland>
1995-01-21 15:19:29 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/i386/sigreturn.c
+++ sysdeps/mach/hurd/i386/sigreturn.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -121,3 +121,5 @@ __sigreturn (struct sigcontext *scp)
   /* NOTREACHED */
   return -1;
 }
+
+weak_alias (__sigreturn, sigreturn)
