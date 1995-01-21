96acf760b809aa695837bc30238f40f55834de8f
roland <roland>
1995-01-21 15:08:43 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- hurd/setauth.c
+++ hurd/setauth.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -121,3 +121,5 @@ __setauth (auth_t new)
   error_t err = _hurd_setauth (new);
   return err ? __hurd_fail (err) : 0;
 }
+
+weak_alias (__setauth, setauth)
