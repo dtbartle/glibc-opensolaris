b37bd4d20eb161205c39dc8b3dab8c163676e38b
roland <roland>
1995-01-21 15:28:07 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/unlink.c
+++ sysdeps/mach/hurd/unlink.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -42,3 +42,5 @@ DEFUN(__unlink, (name), CONST char *name)
     return __hurd_fail (err);
   return 0;
 }
+
+weak_alias (__unlink, unlink)
