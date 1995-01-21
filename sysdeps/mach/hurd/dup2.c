59a591e7f87e6dc7959f3b127e0d90936044bcd0
roland <roland>
1995-01-21 14:31:10 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/dup2.c
+++ sysdeps/mach/hurd/dup2.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -111,3 +111,5 @@ DEFUN(__dup2, (fd, fd2), int fd AND int fd2)
 
   return fd2;
 }
+
+weak_alias (__dup2, dup2)
