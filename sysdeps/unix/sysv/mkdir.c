7cf33b42d86fd78d863825e54922bae75c3ab109
roland <roland>
1995-01-21 14:58:46 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/unix/sysv/mkdir.c
+++ sysdeps/unix/sysv/mkdir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -93,3 +93,5 @@ DEFUN(__mkdir, (path, mode), CONST char *path AND mode_t mode)
   else
     return -1;
 }
+
+weak_alias (__mkdir, mkdir)
