af8a32f76a54f0e6064a5025bf30c2c7f2431315
roland <roland>
1995-01-21 14:22:23 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/adjtime.c
+++ sysdeps/mach/hurd/adjtime.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -49,3 +49,5 @@ DEFUN(__adjtime, (delta, olddelta),
     return __hurd_fail (err);
   return 0;
 }
+
+weak_alias (__adjtime, adjtime)
