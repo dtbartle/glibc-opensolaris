3f5cdae7e5e324341078de92c987729e155079b1
roland <roland>
1995-01-21 15:19:59 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/posix/sigvec.c
+++ sysdeps/posix/sigvec.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -158,3 +158,5 @@ DEFUN(__sigvec, (sig, vec, ovec),
 
   return 0;
 }
+
+weak_alias (__sigvec, sigvec)
--- sysdeps/stub/sigvec.c
+++ sysdeps/stub/sigvec.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -32,12 +32,6 @@ DEFUN(__sigvec, (sig, vec, ovec),
   errno = ENOSYS;
   return -1;
 }
+stub_warning (sigvec)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__sigvec);
-
-#endif	/* GNU stabs.  */
+weak_alias (__sigvec, sigvec)
