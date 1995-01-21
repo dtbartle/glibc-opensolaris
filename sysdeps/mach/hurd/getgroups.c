0de75cd1c561a15fba49b8e124e1f9bbb1a1683f
roland <roland>
1995-01-21 14:45:42 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/getgroups.c
+++ sysdeps/mach/hurd/getgroups.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -64,3 +64,5 @@ __getgroups (int n, gid_t *gidset)
 
   return ngids;
 }
+
+weak_alias (__getgroups, getgroups)
