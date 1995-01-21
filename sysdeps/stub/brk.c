edc9d0b3ee8b41f26c1407c3742a04a85f51a86f
roland <roland>
1995-01-21 14:22:33 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/brk.c
+++ sysdeps/stub/brk.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -27,12 +27,6 @@ DEFUN(__brk, (addr), PTR addr)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (brk)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__brk);
-
-#endif	/* GNU stabs.  */
+weak_alias (__brk, brk)
