30c976b12102f62845e7f4ecc1f36bc867a5fa9e
roland <roland>
1995-01-21 14:21:44 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/access.c
+++ sysdeps/mach/hurd/access.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -134,3 +134,5 @@ DEFUN(__access, (file, type), CONST char *file AND int type)
 
   return 0;
 }
+
+weak_alias (__access, access)
