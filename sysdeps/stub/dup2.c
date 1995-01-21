2c892bd181358e53fcf7f1a510956ccef586cdc7
roland <roland>
1995-01-21 14:30:26 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/dup2.c
+++ sysdeps/stub/dup2.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -40,12 +40,6 @@ DEFUN(__dup2, (fd, fd2), int fd AND int fd2)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (dup2)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__dup2);
-
-#endif	/* GNU stabs.  */
+weak_alias (__dup2, dup2)
