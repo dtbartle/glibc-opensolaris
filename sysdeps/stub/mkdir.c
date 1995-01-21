b8755a4e2c50f107986c21f7636f8cbd3f91037c
roland <roland>
1995-01-21 14:58:35 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/mkdir.c
+++ sysdeps/stub/mkdir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -36,12 +36,6 @@ DEFUN(__mkdir, (path, mode), CONST char *path AND mode_t mode)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (mkdir)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__mkdir);
-
-#endif	/* GNU stabs.  */
+weak_alias (__mkdir, mkdir)
