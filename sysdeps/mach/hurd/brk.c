8290cb4b13036cdb59459ab5c0390dd1ce5eaf83
roland <roland>
1995-01-21 14:25:32 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/brk.c
+++ sysdeps/mach/hurd/brk.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -127,3 +127,5 @@ init_brk (void)
   (void) &init_brk;		/* Avoid ``defined but not used'' warning.  */
 }
 text_set_element (_hurd_preinit_hook, init_brk);
+
+weak_alias (__brk, brk)
