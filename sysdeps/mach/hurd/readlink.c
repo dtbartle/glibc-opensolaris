f3b3be491bd42d17c4497eab82b55097f94c3948
roland <roland>
1995-01-21 15:05:19 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/readlink.c
+++ sysdeps/mach/hurd/readlink.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -72,3 +72,5 @@ DEFUN(__readlink, (file_name, buf, len),
 
   return err ? __hurd_fail (err) : len;
 }
+
+weak_alias (__readlink, readlink)
