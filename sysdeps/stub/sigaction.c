887d8178dee081c7fcce6688e5c0d98ea526e4ce
roland <roland>
1995-01-21 15:38:49 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/sigaction.c
+++ sysdeps/stub/sigaction.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -36,12 +36,6 @@ DEFUN(__sigaction, (sig, act, oact),
   errno = ENOSYS;
   return(-1);
 }
+stub_warning (sigaction)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__sigaction);
-
-#endif	/* GNU stabs.  */
+weak_alias (__sigaction, sigaction)
--- sysdeps/unix/bsd/sigaction.c
+++ sysdeps/unix/bsd/sigaction.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -58,3 +58,5 @@ DEFUN(__sigaction, (sig, act, oact),
 
   return 0;
 }
+
+weak_alias (__sigaction, sigaction)
