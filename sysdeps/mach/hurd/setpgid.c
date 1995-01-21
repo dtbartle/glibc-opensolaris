402e7de05eb447fd360dfb9a33374c0b8d1163a9
roland <roland>
1995-01-21 15:11:56 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/setpgid.c
+++ sysdeps/mach/hurd/setpgid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -52,3 +52,5 @@ DEFUN(__setpgrp, (pid, pgid), pid_t pid AND pid_t pgid)
   return 0;
 
 }
+
+weak_alias (__setpgrp, setpgrp)
