d31687fc430da088845e203c15ef69d01cd2fa2d
roland <roland>
1995-01-21 15:12:11 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/setreuid.c
+++ sysdeps/stub/setreuid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -30,12 +30,6 @@ DEFUN(__setreuid, (effective_uid, real_uid),
   errno = ENOSYS;
   return -1;
 }
+stub_warning (setreuid)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__setreuid);
-
-#endif	/* GNU stabs.  */
+weak_alias (__setreuid, setreuid)
