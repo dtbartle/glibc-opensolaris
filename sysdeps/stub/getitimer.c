653a98eb8601fa8a5a06e4c2c045babac8d752b2
roland <roland>
1995-01-21 14:46:27 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/getitimer.c
+++ sysdeps/stub/getitimer.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -36,12 +36,6 @@ DEFUN(__getitimer, (which, value),
   errno = ENOSYS;
   return -1;
 }
+stub_warning (getitimer)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__getitimer);
-
-#endif	/* GNU stabs.  */
+weak_alias (__getitimer, getitimer)
