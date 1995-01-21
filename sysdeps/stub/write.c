e64ee012828480fee146b9e0a9ad855d97716363
roland <roland>
1995-01-21 15:37:41 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/write.c
+++ sysdeps/stub/write.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -43,12 +43,6 @@ DEFUN(__write, (fd, buf, nbytes),
   errno = ENOSYS;
   return -1;
 }
+stub_warning (write)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__write);
-
-#endif	/* GNU stabs.  */
+weak_alias (__write, write)
