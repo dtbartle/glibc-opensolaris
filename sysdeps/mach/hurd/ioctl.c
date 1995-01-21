a5aa954d247b5a364beb894e6ccecbd6c130bb79
roland <roland>
1995-01-21 14:50:53 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/ioctl.c
+++ sysdeps/mach/hurd/ioctl.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -296,3 +296,5 @@ DEFUN(__ioctl, (fd, request),
       return __hurd_fail (err);
     }
 }
+
+weak_alias (__ioctl, ioctl)
