17bd91985424e109d61b4c0918595e04d39c79f8
roland <roland>
1995-01-21 14:33:53 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/fcntl.c
+++ sysdeps/stub/fcntl.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -33,12 +33,6 @@ DEFUN(__fcntl, (fd, cmd), int fd AND int cmd DOTS)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (fcntl)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__fcntl);
-
-#endif	/* GNU stabs.  */
+weak_alias (__fcntl, fcntl)
