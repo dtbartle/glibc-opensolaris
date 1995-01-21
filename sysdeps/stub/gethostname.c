9c74c1b0b921d0ea8aed6b936f60a551db19c01f
roland <roland>
1995-01-21 14:45:52 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/gethostname.c
+++ sysdeps/stub/gethostname.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -30,12 +30,6 @@ DEFUN(__gethostname, (name, len),
   errno = ENOSYS;
   return -1;
 }
+stub_warning (gethostname)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__gethostname);
-
-#endif	/* GNU stabs.  */
+weak_alias (__gethostname, gethostname)
--- sysdeps/unix/sysv/gethostname.c
+++ sysdeps/unix/sysv/gethostname.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -34,3 +34,5 @@ DEFUN(__gethostname, (name, len),
   strncpy (name, buf.nodename, len);
   return 0;
 }
+
+weak_alias (__gethostname, gethostname)
