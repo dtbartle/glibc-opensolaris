b448e9bcac50d962f58c7e7882e8a239768a74f6
roland <roland>
1995-01-21 15:28:23 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/utimes.c
+++ sysdeps/stub/utimes.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -36,3 +36,5 @@ DEFUN(__utimes, (file, tvp),
   errno = ENOSYS;
   return -1;
 }
+
+weak_alias (__utimes, utimes)
