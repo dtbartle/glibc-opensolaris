9ff3ce5d736a9b8f5d0ec56696cd026d43035cdd
roland <roland>
1995-01-21 15:08:19 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/select.c
+++ sysdeps/stub/select.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -34,3 +34,6 @@ DEFUN(__select, (nfds, readfds, writefds, exceptfds, timeout),
   errno = ENOSYS;
   return -1;
 }
+stub_warning (select)
+
+weak_alias (__select, select)
