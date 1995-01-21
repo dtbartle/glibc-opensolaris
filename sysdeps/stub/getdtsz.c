db8aadc22842e7bdff528799e5509fcf61d4b348
roland <roland>
1995-01-21 14:42:24 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/getdtsz.c
+++ sysdeps/stub/getdtsz.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -28,12 +28,6 @@ DEFUN_VOID(__getdtablesize)
   errno = ENOSYS;
   return -1;
 }
+stub_warning (getdtablesize)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__getdtablesize);
-
-#endif	/* GNU stabs.  */
+weak_alias (__getdtablesize, getdtablesize)
