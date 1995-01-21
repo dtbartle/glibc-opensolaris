3eff5365f6fba54ab6b5906cc28d479201305bce
roland <roland>
1995-01-21 15:25:32 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/stub/sysconf.c
+++ sysdeps/stub/sysconf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -65,3 +65,5 @@ DEFUN(__sysconf, (name), int name)
   errno = ENOSYS;
   return -1;
 }
+
+weak_alias (__sysconf, sysconf)
