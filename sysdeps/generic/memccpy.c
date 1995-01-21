e24586554162bc157301c22105685f8e05ee4b20
roland <roland>
1995-01-21 14:58:11 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/generic/memccpy.c
+++ sysdeps/generic/memccpy.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -40,3 +40,5 @@ DEFUN(__memccpy, (dest, src, c, n),
 
   return NULL;
 }
+
+weak_alias (__memccpy, memccpy)
