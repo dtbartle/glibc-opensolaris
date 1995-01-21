01ff557ed46a82d57d28b47d6b85d1f14f14113b
roland <roland>
1995-01-21 14:37:47 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- stdio/getdelim.c
+++ stdio/getdelim.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -168,3 +168,5 @@ DEFUN(__getdelim, (lineptr, n, terminator, stream),
   *p = '\0';
   return p - *lineptr;
 }
+
+weak_alias (__getdelim, getdelim)
