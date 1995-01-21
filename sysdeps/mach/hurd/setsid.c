8595cd9d8a8a1d40afddd8e7c163162593f0a416
roland <roland>
1995-01-21 15:15:01 +0000

    Converted to use weak aliases with macros from libc-symbols.h.

--- sysdeps/mach/hurd/setsid.c
+++ sysdeps/mach/hurd/setsid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -55,3 +55,5 @@ DEFUN_VOID(__setsid)
 
   return 0;
 }
+
+weak_alias (__setsid, setsid)
