38dc284273d6878de4844750d6525342d91c4e6c
roland <roland>
1995-01-23 08:32:23 +0000

    (mach_setup_thread): Define as weak alias.

--- mach/setup-thread.c
+++ mach/setup-thread.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -95,3 +95,5 @@ __mach_setup_thread (task_t task, thread_t thread, void *pc,
   return __thread_set_state (thread, MACHINE_THREAD_STATE_FLAVOR,
 			     (int *) &ts, tssize);
 }
+
+weak_alias (__mach_setup_thread, mach_setup_thread)
