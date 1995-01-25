d00b790a6976bbbd9e26518db8b4a95f295c70ef
roland <roland>
1995-01-25 23:17:40 +0000

    Use new libc-symbols.h macro for stub warning.

--- sysdeps/stub/chflags.c
+++ sysdeps/stub/chflags.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -35,11 +35,4 @@ DEFUN(chflags, (file, flags), CONST char *file AND int flags)
   return -1;
 }
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(chflags);
-
-#endif	/* GNU stabs.  */
+stub_warning (chflags)
