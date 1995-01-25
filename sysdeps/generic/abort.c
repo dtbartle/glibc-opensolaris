286ed2c78da3c9ba3e679991042f47ce6ba5b174
roland <roland>
1995-01-25 22:16:55 +0000

    Remove __NORETURN; it's obsolete.

--- sysdeps/generic/abort.c
+++ sysdeps/generic/abort.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -24,7 +24,6 @@ Cambridge, MA 02139, USA.  */
 #include <signal.h>
 
 /* Cause an abnormal program termination with core-dump.  */
-__NORETURN
 void
 DEFUN_VOID(abort)
 {
