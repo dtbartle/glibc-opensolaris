36107b041a858443885e3cc99e2966eefa5a17e4
roland <roland>
1995-01-25 22:46:13 +0000

    Use new libc-symbols.h macro for stub warning.

--- sysdeps/stub/fexecve.c
+++ sysdeps/stub/fexecve.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -39,11 +39,4 @@ DEFUN(fexecve, (fd, argv, envp),
   return -1;
 }
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(fexecve);
-
-#endif	/* GNU stabs.  */
+stub_warning (fexecve)
