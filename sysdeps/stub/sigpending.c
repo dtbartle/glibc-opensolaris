340aef1ed87fc652259821dbb2a80d0d7a03cdd4
roland <roland>
1995-01-25 21:40:05 +0000

    Use new libc-symbols.h macro for stub warning.

--- sysdeps/stub/sigpending.c
+++ sysdeps/stub/sigpending.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -36,11 +36,4 @@ DEFUN(sigpending, (set), sigset_t *set)
   return -1;
 }
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(sigpending);
-
-#endif	/* GNU stabs.  */
+stub_warning (sigpending)
