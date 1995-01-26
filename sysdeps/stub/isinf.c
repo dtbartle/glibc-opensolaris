8dcc81b3df2b2925c5cf8cc12ed1c328a854a587
roland <roland>
1995-01-26 03:41:38 +0000

    Use new stub_warning macro. Add weak alias isinf for __isinf.

--- sysdeps/stub/isinf.c
+++ sysdeps/stub/isinf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,12 +26,6 @@ DEFUN(__isinf, (value), double value)
 {
   return 0;
 }
+weak_alias (__isinf, isinf)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__isinf);
-
-#endif	/* GNU stabs.  */
+stub_warning (__isinf)
