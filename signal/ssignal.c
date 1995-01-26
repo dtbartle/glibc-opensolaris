40662d069e74b08693963d770342b227fd27c72e
roland <roland>
1995-01-26 02:21:43 +0000

    Don't use function_alias, just a normal function.

--- signal/ssignal.c
+++ signal/ssignal.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -16,11 +16,14 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <ansidecl.h>
 #include <signal.h>
-#include <gnu-stabs.h>
 
 #undef	ssignal
 
-function_alias(ssignal, signal, __sighandler_t, (sig, func),
-	       DEFUN(ssignal, (sig, handler), int sig AND __sighandler_t func))
+__sighandler_t 
+ssignal (sig, func)
+     int sig;
+     __sighandler_t func;
+{
+  return signal (sig, func);
+}
