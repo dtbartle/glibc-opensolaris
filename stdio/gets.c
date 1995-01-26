15c11773b643a3109e767964bf61869f704e6d80
roland <roland>
1995-01-26 03:25:50 +0000

    Use new link_warning macro instead of old warn_references.

--- stdio/gets.c
+++ stdio/gets.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -21,12 +21,7 @@ Cambridge, MA 02139, USA.  */
 #include <errno.h>
 #include <string.h>
 
-
-#ifdef	HAVE_GNU_LD
-#include <gnu-stabs.h>
-warn_references(gets,
-		"the `gets' function is unreliable and should not be used.");
-#endif
+link_warning ("the `gets' function is unreliable and should not be used.")
 
 /* Read a newline-terminated string from stdin into S,
    removing the trailing newline.  Return S or NULL.  */
