02cd920b06a65af10c5b53405dfe30d40f54a2a1
roland <roland>
1995-01-26 03:36:03 +0000

    Remove obsolete __CONSTVALUE keyword.

--- sysdeps/generic/atan.c
+++ sysdeps/generic/atan.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,7 +20,7 @@ Cambridge, MA 02139, USA.  */
 #include <math.h>
 
 /* Return the inverse tangent of X.  */
-__CONSTVALUE double
+double
 DEFUN(atan, (x), register double x)
 {
   return atan2(x, 1.0);
