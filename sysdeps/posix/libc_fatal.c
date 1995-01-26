0db062f12b09e78186d20eba9b1ba78431fb7d11
roland <roland>
1995-01-26 03:39:47 +0000

    Remove obsolete __NORETURN keyword.

--- sysdeps/posix/libc_fatal.c
+++ sysdeps/posix/libc_fatal.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -23,7 +23,6 @@ Cambridge, MA 02139, USA.  */
 #include <sysdep.h>
 
 /* Abort with an error message.  */
-__NORETURN
 void
 DEFUN(__libc_fatal, (message), CONST char *message)
 {
