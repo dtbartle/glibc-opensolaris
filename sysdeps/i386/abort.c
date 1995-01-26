c4f6cec27a9ef404d3101e53974b7324583fc6ce
roland <roland>
1995-01-26 03:38:26 +0000

    Remove obsolete __NORETURN keyword.

--- sysdeps/i386/abort.c
+++ sysdeps/i386/abort.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,7 +20,6 @@ Cambridge, MA 02139, USA.  */
 #include <stdlib.h>
 
 /* Cause an abnormal program termination with core-dump.  */
-__NORETURN
 void
 DEFUN_VOID(abort)
 {
