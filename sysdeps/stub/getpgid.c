03166afae5abadb2fed2b9079b4a21792ed514b3
roland <roland>
1995-01-26 01:15:24 +0000

    Renamed __getpgrp to __getpgid, added weak alias getpgid.

--- sysdeps/stub/getpgid.c
+++ sysdeps/stub/getpgid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -23,16 +23,10 @@ Cambridge, MA 02139, USA.  */
 
 /* Get the process group ID of process PID.  */
 int
-DEFUN(__getpgrp, (pid), pid_t pid)
+DEFUN(__getpgid, (pid), pid_t pid)
 {
   return pid;
 }
+weak_alias (__getpgid, getpgid)
 
-
-#ifdef	 HAVE_GNU_LD
-
-#include <gnu-stabs.h>
-
-stub_warning(__getpgrp);
-
-#endif	/* GNU stabs.  */
+stub_warning (getpgid)
