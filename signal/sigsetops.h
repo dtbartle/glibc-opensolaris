1ee191f80517180ae23d44d7280f6c47f19febe4
roland <roland>
1995-01-26 02:22:43 +0000

    Don't include gnu-stabs.h.

--- signal/sigsetops.h
+++ signal/sigsetops.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -32,5 +32,3 @@ Cambridge, MA 02139, USA.  */
 #undef	sigaddset
 #undef	sigdelset
 #undef	sigismember
-
-#include <gnu-stabs.h>
