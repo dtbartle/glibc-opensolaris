0d1f845dd3b31ce22587321aac28cb029f2e3fc5
roland <roland>
1995-01-26 04:19:10 +0000

    Don't include gnu-stabs.h.

--- hurd/hurdid.c
+++ hurd/hurdid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -18,7 +18,6 @@ Cambridge, MA 02139, USA.  */
 
 #include <hurd.h>
 #include <hurd/id.h>
-#include <gnu-stabs.h>
 
 struct hurd_id_data _hurd_id;
 
