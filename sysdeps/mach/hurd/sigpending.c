2df945d9ded0aee0629c67d476625944965e2872
roland <roland>
1995-02-07 04:18:00 +0000

    Use spin lock operations on sigstate lock. Don't expect _hurd_self_sigstate to lock it.

--- sysdeps/mach/hurd/sigpending.c
+++ sysdeps/mach/hurd/sigpending.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1994, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -37,8 +37,9 @@ DEFUN(sigpending, (set), sigset_t *set)
     }
 
   ss = _hurd_self_sigstate ();
+  __spin_lock (&ss->lock);
   pending = ss->pending;
-  __mutex_unlock (&ss->lock);
+  __spin_unlock (&ss->lock);
 
   *set = pending;
   return 0;
