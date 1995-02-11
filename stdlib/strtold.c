41c1d449f45f992c1dee73b7c8e4b387953884dc
roland <roland>
1995-02-11 18:12:45 +0000

    Initial revision

---
+++ stdlib/strtold.c
@@ -0,0 +1,9 @@
+/* The actual implementation for all floating point sizes is in strtod.c.
+   These macros tell it to produce the `long double' version, `strtold'.  */
+
+#define	FLOAT		long double
+#define	FLT		LDBL
+#define	STRTOF		strtold
+#define	MPN2FLOAT	__mpn_construct_long_double
+
+#include "strtod.c"
