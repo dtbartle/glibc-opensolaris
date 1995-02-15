ce5dde4f1256e18407217301dbaddc171a0560dc
roland <roland>
1995-02-15 21:48:48 +0000

    Don't lock the file descriptor; HURD_FD_PORT_USE will. Don't use critical sections around HURD_FD_PORT_USE invocations.

--- sysdeps/mach/hurd/stdio_init.c
+++ sysdeps/mach/hurd/stdio_init.c
@@ -41,10 +41,7 @@ DEFUN(__stdio_init_stream, (stream), FILE *stream)
     return;
 
   /* Find out what sort of file this is.  */
-  HURD_CRITICAL_BEGIN;
-  err = HURD_FD_PORT_USE (d, __io_stat (port, &statb));
-  HURD_CRITICAL_END;
-  if (err)
+  if (err = HURD_FD_PORT_USE (d, __io_stat (port, &statb)))
     return;
 
   if (S_ISCHR (statb.st_mode))
@@ -52,10 +49,7 @@ DEFUN(__stdio_init_stream, (stream), FILE *stream)
       /* It's a character device.
 	 Make it line-buffered if it's a terminal.  */
       mach_port_t cttyid;
-      HURD_CRITICAL_BEGIN;
-      __spin_lock (&d->port.lock);
       err = HURD_FD_PORT_USE (d, __term_getctty (port, &cttyid));
-      HURD_CRITICAL_END;
       if (! err)
 	{
 	  __mach_port_deallocate (__mach_task_self (), cttyid);
