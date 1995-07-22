85fad4591b1d3c8a25f2736333914f206337810d
roland <roland>
1995-07-22 06:32:32 +0000

    New file.

---
+++ sysdeps/posix/utimes.c
@@ -0,0 +1,42 @@
+/* Copyright (C) 1995 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <sys/time.h>
+#include <errno.h>
+#include <stddef.h>
+
+/* Change the access time of FILE to TVP[0] and
+   the modification time of FILE to TVP[1].  */
+int
+__utimes (const char *file, struct timeval tvp[2])
+{
+  struct utimbuf buf, *times;
+
+  if (tvp)
+    {
+      times = &buf;
+      times->actime = tvp[0].tv_sec + tvp[0].tv_usec / 1000;
+      times->modtime = tvp[1].tv_sec + tvp[1].tv_usec / 1000;
+    }
+  else
+    times = NULL;
+
+  return utime (path, times);
+}
+
+weak_alias (__utimes, utimes)
