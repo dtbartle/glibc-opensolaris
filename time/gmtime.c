ac1a245c2931dbc707ea9a85dc99d816fca4545d
roland <roland>
1995-09-18 18:55:32 +0000

    (gmtime_r): Define as weak alias.

--- time/gmtime.c
+++ time/gmtime.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1995 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -44,3 +44,4 @@ DEFUN(__gmtime_r, (t, tp),
 
   return tp;
 }
+weak_alias (__gmtime_r, gmtime_r)
