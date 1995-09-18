e822c7da5d53763fe7468b6d78b48fbd3e543fd6
roland <roland>
1995-09-18 18:15:22 +0000

    Sun Sep 17 08:22:12 1995  Paul Eggert  <eggert@twinsun.com>
    	* time/localtime.c: <stddef.h>, <ctype.h>, <stdio.h>,
    	<stdlib.h>, <string.h>: Remove includes.
    	<errno.h>: Add include.

--- time/localtime.c
+++ time/localtime.c
@@ -16,11 +16,7 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <stddef.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <errno.h>
 #include <time.h>
 
 
