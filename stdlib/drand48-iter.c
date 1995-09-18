27fa1866699d35f245cbbf7c50b4c3946b56ba8b
roland <roland>
1995-09-18 06:01:18 +0000

    (__drand48_iterate): Use U suffix for huge constant.

--- stdlib/drand48-iter.c
+++ stdlib/drand48-iter.c
@@ -48,8 +48,8 @@ __drand48_iterate (xsubi, buffer)
 	}
       else
 	{
-	  buffer->a[2] = 0x5deec;
-	  buffer->a[1] = 0xe66d0000;
+	  buffer->a[2] = 0x5deecU;
+	  buffer->a[1] = 0xe66d0000U;
 	  buffer->a[0] = 0;
 	}
       buffer->c = 0xb;
