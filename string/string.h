222b2a897335367cf3e50edaa4e5a7120e94c423
roland <roland>
1995-09-27 16:45:25 +0000

    Declare __strcasecmp.

--- string/string.h
+++ string/string.h
@@ -149,6 +149,7 @@ extern int bcmp __P ((__const __ptr_t __s1, __const __ptr_t __s2, size_t __n));
 extern int ffs __P ((int __i));
 
 /* Compare S1 and S2, ignoring case.  */
+extern int __strcasecmp __P ((__const char *__s1, __const char *__s2));
 extern int strcasecmp __P ((__const char *__s1, __const char *__s2));
 
 /* Return the next DELIM-delimited token from *STRINGP,
