e90ce332dfbd4b5f49b3fdc9ffe5884112e5d321
roland <roland>
1996-05-27 20:14:49 +0000

    Mon May 27 10:10:00 1996  Roland McGrath  <roland@delasyd.gnu.ai.mit.edu>
    	* sysdeps/unix/sysv/linux/ioctl-types.h: New file, wrapper around
    	kernel header <asm/termios.h>.

---
+++ sysdeps/unix/sysv/linux/ioctl-types.h
@@ -0,0 +1,27 @@
+/* Structure types for pre-termios terminal ioctls.  Linux version.
+Copyright (C) 1996 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#ifndef _IOCTL_TYPES_H
+#define _IOCTL_TYPES_H 1
+
+/* The Linux kernel header defines the types associated with old ioctls.  */
+
+#include <asm/termios.h>
+
+#endif /* ioctl-types.h */
