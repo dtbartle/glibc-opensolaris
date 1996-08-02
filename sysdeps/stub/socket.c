b6f46354bb09b66a29d451022251f5116bedb44b
roland <roland>
1996-08-02 04:42:53 +0000

    Thu Aug  1 14:40:03 1996  Roland McGrath  <roland@fast.cs.utah.edu>
    	* sysdeps/stub/socket.c: Fix type of TYPE arg.
    	* sysdeps/stub/socketpair.c: Likewise.

--- sysdeps/stub/socket.c
+++ sysdeps/stub/socket.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995, 1996 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -25,7 +25,7 @@ Cambridge, MA 02139, USA.  */
    Returns a file descriptor for the new socket, or -1 for errors.  */
 int
 DEFUN(socket, (domain, type, protocol),
-      int domain AND enum __socket_type type AND int protocol)
+      int domain AND int type AND int protocol)
 {
   errno = ENOSYS;
   return -1;
