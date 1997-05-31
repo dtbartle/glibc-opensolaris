9e2fb8cfc49da6c6a740988a184c686519c170e4
drepper <drepper>
1997-05-31 00:44:21 +0000

    Include only <string.h>.

--- sysdeps/generic/memccpy.c
+++ sysdeps/generic/memccpy.c
@@ -16,7 +16,6 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-#include <stddef.h>	/* For size_t and NULL.	*/
 #include <string.h>
 
 /*
