5f8bd8c04c015d10a4686e391e63bca200b07f00
drepper <drepper>
1997-06-21 02:14:22 +0000

    Moved to bits/.

--- sysdeps/wordsize-32/elfclass.h
+++
@@ -1,5 +0,0 @@
-/* This file specifies the native word size of the machine, which indicates
-   the ELF file class used for executables and shared objects on this
-   machine.  */
-
-#define __ELF_NATIVE_CLASS 32
