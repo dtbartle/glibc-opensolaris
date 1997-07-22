e8e7fed07348c4ee07490d1a2a5f80720502d010
drepper <drepper>
1997-07-22 09:47:38 +0000

    (nss_lookup_function): Inclide function.def, not functions.def.

--- nss/nsswitch.c
+++ nss/nsswitch.c
@@ -426,7 +426,7 @@ nss_lookup_function (service_user *ni, const char *fct_name)
 	    extern void _nss_##h##_get##nm##_r (void);
 # define DEFINE_GETBY(h,nm,ky)						      \
 	    extern void _nss_##h##_get##nm##by##ky##_r (void);
-# include "functions.def"
+# include "function.def"
 # undef DEFINE_ENT
 # undef DEFINE_GET
 # undef DEFINE_GETBY
@@ -440,7 +440,7 @@ nss_lookup_function (service_user *ni, const char *fct_name)
 	    { #h"_get"#nm"by"#ky"_r", _nss_##h##_get##nm##by##ky##_r },
 	    static struct fct_tbl { const char *fname; void *fp; } *tp, tbl[] =
 	      {
-# include "functions.def"
+# include "function.def"
 		{ NULL, NULL }
 	      };
 	    size_t namlen = (5 + strlen (ni->library->name) + 1
