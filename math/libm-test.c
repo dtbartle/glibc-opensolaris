2d0db969de5418a8e1b59a5f47e6d20dabbd3aa5
drepper <drepper>
1997-10-06 00:58:59 +0000

    Add test for clog10.

--- math/libm-test.c
+++ math/libm-test.c
@@ -4301,6 +4301,160 @@ clog_test (void)
 
 
 static void
+clog10_test (void)
+{
+  __complex__ MATHTYPE result;
+
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_zero, 0));
+  check_isinfn_exc ("real(clog10(-0 + i0)) = -Inf plus divide-by-zero exception",
+		    __real__ result, DIVIDE_BY_ZERO_EXCEPTION);
+  check ("imag(clog10(-0 + i0)) = pi plus divide-by-zero exception",
+	 __imag__ result, M_PI);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_zero, minus_zero));
+  check_isinfn_exc ("real(clog10(-0 - i0)) = -Inf plus divide-by-zero exception",
+		    __real__ result, DIVIDE_BY_ZERO_EXCEPTION);
+  check ("imag(clog10(-0 - i0)) = -pi plus divide-by-zero exception",
+	 __imag__ result, -M_PI);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (0, 0));
+  check_isinfn_exc ("real(clog10(0 + i0)) = -Inf plus divide-by-zero exception",
+		    __real__ result, DIVIDE_BY_ZERO_EXCEPTION);
+  check ("imag(clog10(0 + i0)) = 0 plus divide-by-zero exception",
+	 __imag__ result, 0);
+  result = FUNC(clog10) (BUILD_COMPLEX (0, minus_zero));
+  check_isinfn_exc ("real(clog10(0 - i0)) = -Inf plus divide-by-zero exception",
+		    __real__ result, DIVIDE_BY_ZERO_EXCEPTION);
+  check ("imag(clog10(0 - i0)) = -0 plus divide-by-zero exception",
+	 __imag__ result, minus_zero);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_infty, plus_infty));
+  check_isinfp ("real(clog10(-Inf + i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(-Inf + i Inf)) = 3*pi/4", __imag__ result, M_PI - M_PI_4);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_infty, minus_infty));
+  check_isinfp ("real(clog10(-Inf - i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(-Inf - i Inf)) = -3*pi/4", __imag__ result, M_PI_4 - M_PI);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (plus_infty, plus_infty));
+  check_isinfp ("real(clog10(+Inf + i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(+Inf + i Inf)) = pi/4", __imag__ result, M_PI_4);
+  result = FUNC(clog10) (BUILD_COMPLEX (plus_infty, minus_infty));
+  check_isinfp ("real(clog10(+Inf - i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(+Inf - i Inf)) = -pi/4", __imag__ result, -M_PI_4);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (0, plus_infty));
+  check_isinfp ("real(clog10(0 + i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(0 + i Inf)) = pi/2", __imag__ result, M_PI_2);
+  result = FUNC(clog10) (BUILD_COMPLEX (3, plus_infty));
+  check_isinfp ("real(clog10(3 + i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(3 + i Inf)) = pi/2", __imag__ result, M_PI_2);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_zero, plus_infty));
+  check_isinfp ("real(clog10(-0 + i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(-0 + i Inf)) = pi/2", __imag__ result, M_PI_2);
+  result = FUNC(clog10) (BUILD_COMPLEX (-3, plus_infty));
+  check_isinfp ("real(clog10(-3 + i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(-3 + i Inf)) = pi/2", __imag__ result, M_PI_2);
+  result = FUNC(clog10) (BUILD_COMPLEX (0, minus_infty));
+  check_isinfp ("real(clog10(0 - i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(0 - i Inf)) = -pi/2", __imag__ result, -M_PI_2);
+  result = FUNC(clog10) (BUILD_COMPLEX (3, minus_infty));
+  check_isinfp ("real(clog10(3 - i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(3 - i Inf)) = -pi/2", __imag__ result, -M_PI_2);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_zero, minus_infty));
+  check_isinfp ("real(clog10(-0 - i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(-0 - i Inf)) = -pi/2", __imag__ result, -M_PI_2);
+  result = FUNC(clog10) (BUILD_COMPLEX (-3, minus_infty));
+  check_isinfp ("real(clog10(-3 - i Inf)) = +Inf", __real__ result);
+  check ("imag(clog10(-3 - i Inf)) = -pi/2", __imag__ result, -M_PI_2);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_infty, 0));
+  check_isinfp ("real(clog10(-Inf + i0)) = +Inf", __real__ result);
+  check ("imag(clog10(-Inf + i0)) = pi", __imag__ result, M_PI);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_infty, 1));
+  check_isinfp ("real(clog10(-Inf + i1)) = +Inf", __real__ result);
+  check ("imag(clog10(-Inf + i1)) = pi", __imag__ result, M_PI);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_infty, minus_zero));
+  check_isinfp ("real(clog10(-Inf - i0)) = +Inf", __real__ result);
+  check ("imag(clog10(-Inf - i0)) = -pi", __imag__ result, -M_PI);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_infty, -1));
+  check_isinfp ("real(clog10(-Inf - i1)) = +Inf", __real__ result);
+  check ("imag(clog10(-Inf - i1)) = -pi", __imag__ result, -M_PI);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (plus_infty, 0));
+  check_isinfp ("real(clog10(+Inf + i0)) = +Inf", __real__ result);
+  check ("imag(clog10(+Inf + i0)) = 0", __imag__ result, 0);
+  result = FUNC(clog10) (BUILD_COMPLEX (plus_infty, 1));
+  check_isinfp ("real(clog10(+Inf + i1)) = +Inf", __real__ result);
+  check ("imag(clog10(+Inf + i1)) = 0", __imag__ result, 0);
+  result = FUNC(clog10) (BUILD_COMPLEX (plus_infty, minus_zero));
+  check_isinfp ("real(clog10(+Inf - i0)) = +Inf", __real__ result);
+  check ("imag(clog10(+Inf - i0)) = -0", __imag__ result, minus_zero);
+  result = FUNC(clog10) (BUILD_COMPLEX (plus_infty, -1));
+  check_isinfp ("real(clog10(+Inf - i1)) = +Inf", __real__ result);
+  check ("imag(clog10(+Inf - i1)) = -0", __imag__ result, minus_zero);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (plus_infty, nan_value));
+  check_isinfp ("real(clog10(+Inf + i NaN)) = +Inf", __real__ result);
+  check_isnan ("imag(clog10(+Inf + i NaN)) = NaN", __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_infty, nan_value));
+  check_isinfp ("real(clog10(-Inf + i NaN)) = +Inf", __real__ result);
+  check_isnan ("imag(clog10(-Inf + i NaN)) = NaN", __imag__ result);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (nan_value, plus_infty));
+  check_isinfp ("real(clog10(NaN + i Inf)) = +Inf", __real__ result);
+  check_isnan ("imag(clog10(NaN + i Inf)) = NaN", __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (nan_value, minus_infty));
+  check_isinfp ("real(clog10(NaN - i Inf)) = +Inf", __real__ result);
+  check_isnan ("imag(clog10(NaN - i Inf)) = NaN", __imag__ result);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (0, nan_value));
+  check_isnan_maybe_exc ("real(clog10(0 + i NaN)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(0 + i NaN)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (3, nan_value));
+  check_isnan_maybe_exc ("real(clog10(3 + i NaN)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(3 + i NaN)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (minus_zero, nan_value));
+  check_isnan_maybe_exc ("real(clog10(-0 + i NaN)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(-0 + i NaN)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (-3, nan_value));
+  check_isnan_maybe_exc ("real(clog10(-3 + i NaN)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(-3 + i NaN)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (nan_value, 0));
+  check_isnan_maybe_exc ("real(clog10(NaN + i0)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(NaN + i0)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (nan_value, 5));
+  check_isnan_maybe_exc ("real(clog10(NaN + i5)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(NaN + i5)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (nan_value, minus_zero));
+  check_isnan_maybe_exc ("real(clog10(NaN - i0)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(NaN - i0)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+  result = FUNC(clog10) (BUILD_COMPLEX (nan_value, -5));
+  check_isnan_maybe_exc ("real(clog10(NaN - i5)) = NaN plus maybe invalid exception",
+			 __real__ result, INVALID_EXCEPTION);
+  check_isnan ("imag(clog10(NaN - i5)) = NaN plus maybe invalid exception",
+	       __imag__ result);
+
+  result = FUNC(clog10) (BUILD_COMPLEX (nan_value, nan_value));
+  check_isnan ("real(clog10(NaN + i NaN)) = NaN", __real__ result);
+  check_isnan ("imag(clog10(NaN + i NaN)) = NaN", __imag__ result);
+}
+
+
+static void
 csqrt_test (void)
 {
   __complex__ MATHTYPE result;
@@ -4962,6 +5116,7 @@ main (int argc, char *argv[])
   ccos_test ();
   ccosh_test ();
   clog_test ();
+  clog10_test ();
   cacos_test ();
   cacosh_test ();
   casin_test ();
