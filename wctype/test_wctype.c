aad1b2fa8ac76c4526e24c3883de39704958b5e2
drepper <drepper>
1997-12-09 23:59:04 +0000

    Add test for isw* functions.

--- wctype/test_wctype.c
+++ wctype/test_wctype.c
@@ -1,20 +1,20 @@
 /* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+This file is part of the GNU C Library.
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 #include <ctype.h>
 #include <stdio.h>
@@ -48,12 +48,20 @@ main (int argc, char *argv[])
     {
 #define TEST(test)							      \
       do								      \
-	if ((is##test (ch) == 0) != (iswctype (ch, bit_##test)) == 0)	      \
-	  {								      \
-	    printf ("class `%s' test for character \\%o failed\n",	      \
-		    #test, ch);						      \
-	    result = 1;							      \
-	  }								      \
+	{								      \
+	  if ((is##test (ch) == 0) != (iswctype (ch, bit_##test)) == 0)	      \
+	    {								      \
+	      printf ("class `%s' test for character \\%o failed\n",	      \
+		      #test, ch);					      \
+	      result = 1;						      \
+	    }								      \
+	  if ((is##test (ch) == 0) != (isw##test (ch) == 0))		      \
+	    {								      \
+	      printf ("`isw%s' test for character \\%o failed\n",	      \
+		      #test, ch);					      \
+	      result = 1;						      \
+	    }								      \
+	}								      \
       while (0)
 
       TEST (alnum);
