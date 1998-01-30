a67bacd35ea44512fe8fc779b4aa2d2650e9ab22
drepper <drepper>
1998-01-30 11:13:17 +0000

    Remove unneeded inclusion of <errno.h>.

--- misc/init-misc.c
+++ misc/init-misc.c
@@ -1,5 +1,5 @@
 /* Define and initialize `__progname' et. al.
-   Copyright (C) 1994, 1995, 1996, 1997 Free Software Foundation, Inc.
+   Copyright (C) 1994, 1995, 1996, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,7 +18,6 @@
    Boston, MA 02111-1307, USA.  */
 
 #include <string.h>
-#include <errno.h>
 
 char *__progname_full = (char *) "";
 char *__progname = (char *) "";
