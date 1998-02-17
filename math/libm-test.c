56765501f99157d6fe52a02d5389b974a4478f5a
drepper <drepper>
1998-02-17 18:13:34 +0000

    (modf_test): Correct typo.

--- math/libm-test.c
+++ math/libm-test.c
@@ -1703,7 +1703,7 @@ modf_test (void)
   check ("modf (-0, &x) returns -0", result, minus_zero);
   check ("modf (-0, &x) sets x to -0", intpart, minus_zero);
 
-  result = FUNC(modf) (2.5, &intpart);
+  result = FUNC(modf) (1.5, &intpart);
   check ("modf (1.5, &x) returns 0.5", result, 0.5);
   check ("modf (1.5, &x) sets x to 1", intpart, 1);
 
