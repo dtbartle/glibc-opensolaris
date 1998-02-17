8385d259ceebbcc81408a286da8e495cd0adad5f
drepper <drepper>
1998-02-17 16:28:14 +0000

    (modf_test): Add test for 1.5.

--- math/libm-test.c
+++ math/libm-test.c
@@ -1704,6 +1704,10 @@ modf_test (void)
   check ("modf (-0, &x) sets x to -0", intpart, minus_zero);
 
   result = FUNC(modf) (2.5, &intpart);
+  check ("modf (1.5, &x) returns 0.5", result, 0.5);
+  check ("modf (1.5, &x) sets x to 1", intpart, 1);
+
+  result = FUNC(modf) (2.5, &intpart);
   check ("modf (2.5, &x) returns 0.5", result, 0.5);
   check ("modf (2.5, &x) sets x to 2", intpart, 2);
 
