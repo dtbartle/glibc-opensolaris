fe745f49884545aa1c4636ae08a9873fe85b9312
drepper <drepper>
1998-03-02 13:41:58 +0000

    Change all _ISxxx to _ISwxxx.

--- wctype/wcfuncs.c
+++ wctype/wcfuncs.c
@@ -1,20 +1,20 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+/* Copyright (C) 1996, 1998 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 #define	__NO_WCTYPE
 #include <wctype.h>
@@ -25,17 +25,17 @@
 #define	func(name, type) \
   int name (wc) wint_t wc; { return iswctype (wc, type); }
 
-func (iswalnum, _ISalnum)
-func (iswalpha, _ISalpha)
-func (iswcntrl, _IScntrl)
-func (iswdigit, _ISdigit)
-func (iswlower, _ISlower)
-func (iswgraph, _ISgraph)
-func (iswprint, _ISprint)
-func (iswpunct, _ISpunct)
-func (iswspace, _ISspace)
-func (iswupper, _ISupper)
-func (iswxdigit, _ISxdigit)
+func (iswalnum, _ISwalnum)
+func (iswalpha, _ISwalpha)
+func (iswcntrl, _ISwcntrl)
+func (iswdigit, _ISwdigit)
+func (iswlower, _ISwlower)
+func (iswgraph, _ISwgraph)
+func (iswprint, _ISwprint)
+func (iswpunct, _ISwpunct)
+func (iswspace, _ISwspace)
+func (iswupper, _ISwupper)
+func (iswxdigit, _ISwxdigit)
 
 wint_t
 towlower (wc)
