de8e345bd90a2fd3c32d5afab98f5957f14b86c4
drepper <drepper>
1998-03-02 13:42:25 +0000

    (_ISwxxx): Renamed from _ISxxx, all uses changed.  They are incompatible with the _ISxxx values from <ctype.h> on little endian machines. (_ISwbit) [__BYTE_ORDER == __LITTLE_ENDIAN]: Correctly transform bit number.  This fixes the real bug and restores the integrity of the LC_CTYPE locale file.

--- wctype/wctype.h
+++ wctype/wctype.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -55,7 +55,7 @@ typedef unsigned long int wctype_t;
 #define WEOF (0xffffffffu)
 #endif
 
-#ifndef _ISbit
+#ifndef _ISwbit
 /* These are all the characteristics of characters.
    If there get to be more than 16 distinct characteristics,
    many things must be changed that use `unsigned short int's.
@@ -66,27 +66,27 @@ typedef unsigned long int wctype_t;
 
 #include <endian.h>
 #if __BYTE_ORDER == __BIG_ENDIAN
-#define _ISbit(bit)	(1 << bit)
+#define _ISwbit(bit)	(1 << bit)
 #else /* __BYTE_ORDER == __LITTLE_ENDIAN */
-#define _ISbit(bit)	(bit < 8 ? ((1 << bit) << 8) : ((1 << bit) >> 8))
+#define _ISwbit(bit)	(bit < 8 ? 1UL << 24 << bit : 1UL << 8 << bit)
 #endif
 
 enum
 {
-  _ISupper = _ISbit (0),	/* UPPERCASE.  */
-  _ISlower = _ISbit (1),	/* lowercase.  */
-  _ISalpha = _ISbit (2),	/* Alphabetic.  */
-  _ISdigit = _ISbit (3),	/* Numeric.  */
-  _ISxdigit = _ISbit (4),	/* Hexadecimal numeric.  */
-  _ISspace = _ISbit (5),	/* Whitespace.  */
-  _ISprint = _ISbit (6),	/* Printing.  */
-  _ISgraph = _ISbit (7),	/* Graphical.  */
-  _ISblank = _ISbit (8),	/* Blank (usually SPC and TAB).  */
-  _IScntrl = _ISbit (9),	/* Control character.  */
-  _ISpunct = _ISbit (10),	/* Punctuation.  */
-  _ISalnum = _ISbit (11)	/* Alphanumeric.  */
+  _ISwupper = _ISwbit (0),	/* UPPERCASE.  */
+  _ISwlower = _ISwbit (1),	/* lowercase.  */
+  _ISwalpha = _ISwbit (2),	/* Alphabetic.  */
+  _ISwdigit = _ISwbit (3),	/* Numeric.  */
+  _ISwxdigit = _ISwbit (4),	/* Hexadecimal numeric.  */
+  _ISwspace = _ISwbit (5),	/* Whitespace.  */
+  _ISwprint = _ISwbit (6),	/* Printing.  */
+  _ISwgraph = _ISwbit (7),	/* Graphical.  */
+  _ISwblank = _ISwbit (8),	/* Blank (usually SPC and TAB).  */
+  _ISwcntrl = _ISwbit (9),	/* Control character.  */
+  _ISwpunct = _ISwbit (10),	/* Punctuation.  */
+  _ISwalnum = _ISwbit (11)	/* Alphanumeric.  */
 };
-#endif /* Not _ISbit  */
+#endif /* Not _ISwbit  */
 
 
 /*
@@ -180,129 +180,30 @@ extern wint_t towctrans __P ((wint_t __wc, wctrans_t __desc));
 
 
 #ifndef	__NO_WCTYPE
-#define	iswalnum(wc)	__iswctype ((wc), _ISalnum)
-#define	iswalpha(wc)	__iswctype ((wc), _ISalpha)
-#define	iswcntrl(wc)	__iswctype ((wc), _IScntrl)
-#define	iswdigit(wc)	__iswctype ((wc), _ISdigit)
-#define	iswlower(wc)	__iswctype ((wc), _ISlower)
-#define	iswgraph(wc)	__iswctype ((wc), _ISgraph)
-#define	iswprint(wc)	__iswctype ((wc), _ISprint)
-#define	iswpunct(wc)	__iswctype ((wc), _ISpunct)
-#define	iswspace(wc)	__iswctype ((wc), _ISspace)
-#define	iswupper(wc)	__iswctype ((wc), _ISupper)
-#define	iswxdigit(wc)	__iswctype ((wc), _ISxdigit)
+#define	iswalnum(wc)	__iswctype ((wc), _ISwalnum)
+#define	iswalpha(wc)	__iswctype ((wc), _ISwalpha)
+#define	iswcntrl(wc)	__iswctype ((wc), _ISwcntrl)
+#define	iswdigit(wc)	__iswctype ((wc), _ISwdigit)
+#define	iswlower(wc)	__iswctype ((wc), _ISwlower)
+#define	iswgraph(wc)	__iswctype ((wc), _ISwgraph)
+#define	iswprint(wc)	__iswctype ((wc), _ISwprint)
+#define	iswpunct(wc)	__iswctype ((wc), _ISwpunct)
+#define	iswspace(wc)	__iswctype ((wc), _ISwspace)
+#define	iswupper(wc)	__iswctype ((wc), _ISwupper)
+#define	iswxdigit(wc)	__iswctype ((wc), _ISwxdigit)
 
 #ifdef	__USE_GNU
-#define	iswblank(wc)	__iswctype ((wc), _ISblank)
+#define	iswblank(wc)	__iswctype ((wc), _ISwblank)
 #endif
 
 /* Pointer to conversion tables.  */
 extern __const int *__ctype_tolower; /* Case conversions.  */
 extern __const int *__ctype_toupper; /* Case conversions.  */
 
-#define	towlower(wc)	towctrans ((wc), __ctype_tolower)
-#define	towupper(wc)	towctrans ((wc), __ctype_toupper)
+#define	towlower(wc)	towctrans (wc, __ctype_tolower)
+#define	towupper(wc)	towctrans (wc, __ctype_toupper)
 
 #endif /* Not __NO_WCTYPE.  */
-
-#ifdef __USE_GNU
-/* Declare the interface to extended locale model.  */
-# include <xlocale.h>
-
-/* Test for any wide character for which `iswalpha' or `iswdigit' is
-   true.  */
-extern int __iswalnum_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any wide character for which `iswupper' or 'iswlower' is
-   true, or any wide character that is one of a locale-specific set of
-   wide-characters for which none of `iswcntrl', `iswdigit',
-   `iswpunct', or `iswspace' is true.  */
-extern int __iswalpha_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any control wide character.  */
-extern int __iswcntrl_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any wide character that corresponds to a decimal-digit
-   character.  */
-extern int __iswdigit_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any wide character for which `iswprint' is true and
-   `iswspace' is false.  */
-extern int __iswgraph_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any wide character that corresponds to a lowercase letter
-   or is one of a locale-specific set of wide characters for which
-   none of `iswcntrl', `iswdigit', `iswpunct', or `iswspace' is true.  */
-extern int __iswlower_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any printing wide character.  */
-extern int __iswprint_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any printing wide character that is one of a
-   locale-specific et of wide characters for which neither `iswspace'
-   nor `iswalnum' is true.  */
-extern int __iswpunct_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any wide character that corresponds to a locale-specific
-   set of wide characters for which none of `iswalnum', `iswgraph', or
-   `iswpunct' is true.  */
-extern int __iswspace_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any wide character that corresponds to an uppercase letter
-   or is one of a locale-specific set of wide character for which none
-   of `iswcntrl', `iswdigit', `iswpunct', or `iswspace' is true.  */
-extern int __iswupper_l __P ((wint_t __wc, __locale_t __locale));
-
-/* Test for any wide character that corresponds to a hexadecimal-digit
-   character equivalent to that performed be the functions described
-   in the previous subclause.  */
-extern int __iswxdigit_l __P ((wint_t __wc, __locale_t __locale));
-
-
-/* Determine whether the wide-character WC has the property described by
-   DESC.  */
-extern int __iswctype_l __P ((wint_t __wc, wctype_t __desc,
-			      __locale_t locale));
-
-
-/*
- * Wide-character case-mapping functions: 7.15.3.1.
- */
-
-/* Converts an uppercase letter to the corresponding lowercase letter.  */
-extern wint_t __towlower_l __P ((wint_t __wc, __locale_t locale));
-
-/* Converts an lowercase letter to the corresponding uppercase letter.  */
-extern wint_t __towupper_l __P ((wint_t __wc, __locale_t locale));
-
-/* Map the wide character WC using the mapping described by DESC.  */
-extern wint_t __towctrans_l __P ((wint_t __wc, wctrans_t __desc,
-				  __locale_t locale));
-
-
-#ifndef	__NO_WCTYPE
-#define	__iswalnum_l(wc, loc)	__iswctype_l ((wc), _ISalnum, (loc))
-#define	__iswalpha_l(wc, loc)	__iswctype_l ((wc), _ISalpha, (loc))
-#define	__iswcntrl_l(wc, loc)	__iswctype_l ((wc), _IScntrl, (loc))
-#define	__iswdigit_l(wc, loc)	__iswctype_l ((wc), _ISdigit, (loc))
-#define	__iswlower_l(wc, loc)	__iswctype_l ((wc), _ISlower, (loc))
-#define	__iswgraph_l(wc, loc)	__iswctype_l ((wc), _ISgraph, (loc))
-#define	__iswprint_l(wc, loc)	__iswctype_l ((wc), _ISprint, (loc))
-#define	__iswpunct_l(wc, loc)	__iswctype_l ((wc), _ISpunct, (loc))
-#define	__iswspace_l(wc, loc)	__iswctype_l ((wc), _ISspace, (loc))
-#define	__iswupper_l(wc, loc)	__iswctype_l ((wc), _ISupper, (loc))
-#define	__iswxdigit_l(wc, loc)	__iswctype_l ((wc), _ISxdigit, (loc))
-
-#define	__iswblank_l(wc, loc)	__iswctype_l ((wc), _ISblank, (loc))
-
-#define	__towlower_l(wc, loc)	__towctrans_l ((wc), (loc)->__ctype_tolower, \
-					       (loc))
-#define	__towupper_l(wc, loc)	__towctrans_l ((wc), (loc)->__ctype_toupper, \
-					       (loc))
-
-#endif /* Not __NO_WCTYPE.  */
-
-#endif /* Use GNU.  */
 
 __END_DECLS
 
