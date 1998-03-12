a912d387a0d393f4660215e27b610425bad2949a
drepper <drepper>
1998-03-12 16:19:23 +0000

    Fix concurrency problems with error string and number.

--- elf/dlerror.c
+++ elf/dlerror.c
@@ -22,14 +22,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <bits/libc-lock.h>
 
-static int last_errcode;
-static char *last_errstring;
+
+/* Type for storing results of dynamic loading actions.  */
+struct dl_action_result
+  {
+    int errcode;
+    char *errstring;
+  };
+static struct dl_action_result last_result;
+static struct dl_action_result *static_buf;
+
+
+/* This is the key for the thread specific memory.  */
+static __libc_key_t key;
+
+/* Destructor for the thread-specific data.  */
+static void init (void);
+static void free_key_mem (void *mem);
+
 
 char *
 dlerror (void)
 {
   static char *buf;
+  struct dl_action_result *result;
 
   if (buf)
     {
@@ -37,23 +55,33 @@ dlerror (void)
       buf = NULL;
     }
 
-  if (! last_errstring)
+  /* Get error string.  */
+  if (__libc_internal_tsd_get != NULL)
+    {
+      result = (struct dl_action_result *) __libc_getspecific (key);
+      if (result == NULL)
+	result = &last_result;
+    }
+  else
+    result = &last_result;
+
+  if (! result->errstring)
     return NULL;
 
-  if (last_errcode == 0)
-    buf = last_errstring;
+  if (result->errcode == 0)
+    buf = result->errstring;
   else
     {
       if (asprintf (&buf, "%s: %s",
-		    last_errstring, strerror (last_errcode)) == -1)
+		    result->errstring, strerror (result->errcode)) == -1)
 	buf = NULL;
 
       /* We don't need the error string anymore.  */
-      free (last_errstring);
+      free (result->errstring);
     }
 
   /* Reset the error indicator.  */
-  last_errstring = NULL;
+  result->errstring = NULL;
 
   return buf;
 }
@@ -61,11 +89,62 @@ dlerror (void)
 int
 _dlerror_run (void (*operate) (void *), void *args)
 {
-  if (last_errstring != NULL)
+  __libc_once_define (static, once);
+  struct dl_action_result *result;
+
+  /* If we have not yet initialized the buffer do it now.  */
+  __libc_once (once, init);
+
+  /* Get error string and number.  */
+  if (static_buf != NULL)
+    result = static_buf;
+  else
+    {
+      /* We don't use the static buffer and so we have a key.  Use it
+	 to get the thread-specific buffer.  */
+      result = __libc_getspecific (key);
+      if (result == NULL)
+	{
+	  result = (struct dl_action_result *) calloc (1, sizeof (*result));
+	  if (result == NULL)
+	    /* We are out of memory.  Since this is no really critical
+	       situation we carry on by using the global variable.
+	       This might lead to conflicts between the threads but
+	       they soon all will have memory problems.  */
+	    result = &last_result;
+	  else
+	    /* Set the tsd.  */
+	    __libc_setspecific (key, result);
+	}
+    }
+
+  if (result->errstring != NULL)
     /* Free the error string from the last failed command.  This can
        happen if `dlerror' was not run after an error was found.  */
-    free (last_errstring);
+    free (result->errstring);
 
-  last_errcode = _dl_catch_error (&last_errstring, operate, args);
-  return last_errstring != NULL;
+  result->errcode = _dl_catch_error (&result->errstring, operate, args);
+
+  return result->errstring != NULL;
+}
+
+
+/* Initialize buffers for results.  */
+static void
+init (void)
+{
+  if (__libc_key_create (&key, free_key_mem))
+    /* Creating the key failed.  This means something really went
+       wrong.  In any case use a static buffer which is better than
+       nothing.  */
+    static_buf = &last_result;
+}
+
+
+/* Free the thread specific data, this is done if a thread terminates.  */
+static void
+free_key_mem (void *mem)
+{
+  free (mem);
+  __libc_setspecific (key, NULL);
 }
