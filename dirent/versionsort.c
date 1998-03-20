ca47b22e705db9dc9e575b7407a0f2670f9d8627
drepper <drepper>
1998-03-20 12:02:53 +0000

    Pretty print.

--- dirent/versionsort.c
+++ dirent/versionsort.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -23,5 +23,5 @@ int
 versionsort (const void *a, const void *b)
 {
   return strverscmp ((*(const struct dirent **) a)->d_name,
-		 (*(const struct dirent **) b)->d_name);
+		     (*(const struct dirent **) b)->d_name);
 }
