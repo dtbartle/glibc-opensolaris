2fe504d3591f080992a04e2ba329c99586d8c33a
drepper <drepper>
1998-06-07 13:47:02 +0000

    Call _IO_cleanup_region_end with 0 and call _IO_funlockfile explicitly.

--- libio/putchar.c
+++ libio/putchar.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995, 1996, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -30,6 +30,7 @@ putchar (c)
 			       _IO_stdout);
   _IO_flockfile (_IO_stdout);
   result = _IO_putc_unlocked (c, _IO_stdout);
-  _IO_cleanup_region_end (1);
+  _IO_funlockfile (_IO_stdout);
+  _IO_cleanup_region_end (0);
   return result;
 }
