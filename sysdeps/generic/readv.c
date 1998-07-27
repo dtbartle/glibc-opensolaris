0f6d63450ef90b9cbd828fec22242940781c25a0
drepper <drepper>
1998-07-27 17:53:22 +0000

    Correct return type.

--- sysdeps/generic/readv.c
+++ sysdeps/generic/readv.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995, 1996, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -25,7 +25,7 @@
    The buffers are filled in the order specified.
    Operates just like `read' (see <unistd.h>) except that data are
    put in VECTOR instead of a contiguous buffer.  */
-int
+ssize_t
 __readv (fd, vector, count)
      int fd;
      const struct iovec *vector;
