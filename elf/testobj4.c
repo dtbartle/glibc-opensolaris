5d4d484f5f5fd36f3e66d0542ce383666193b05f
drepper <drepper>
1999-01-20 00:33:09 +0000

    Test module for ELF dynamic loading test suite.

---
+++ elf/testobj4.c
@@ -0,0 +1,22 @@
+#include <dlfcn.h>
+
+int
+obj4func1 (int a __attribute__ ((unused)))
+{
+  return 55;
+}
+
+int
+obj4func2 (int a)
+{
+  return foo (a) + 43;
+}
+
+int
+preload (int a)
+{
+  int (*fp) (int) = dlsym (RTLD_NEXT, "preload");
+  if (fp != NULL)
+    return fp (a) + 10;
+  return 10;
+}
