7a154e628fed9c95b024b89cbff80400a038eabf
drepper <drepper>
1999-02-07 12:43:21 +0000

    Add alias _Exit.

--- sysdeps/generic/_exit.c
+++ sysdeps/generic/_exit.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1994, 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 94, 95, 96, 97, 99 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -29,6 +29,7 @@ _exit (status)
   status &= 0xff;
   abort ();
 }
+weak_alias (_exit, _Exit)
 
 stub_warning (_exit)
 #include <stub-tag.h>
