d7fa13d2288b4efe5491246f8e0c2126893ff79d
roland <roland>
1999-06-22 23:54:54 +0000

    1999-06-23  Roland McGrath  <roland@baalperazim.frob.com> 	* hurd/fd-read.c (_hurd_fd_read): Take new parameter OFFSET and 	pass it in the RPC instead of always -1. 	* hurd/fd-write.c (_hurd_fd_write): Likewise. 	* hurd/hurd/fd.h: Update decls.

--- hurd/fd-write.c
+++ hurd/fd-write.c
@@ -1,5 +1,5 @@
 /* _hurd_fd_write -- write to a file descriptor; handles job control et al.
-   Copyright (C) 1993, 1994, 1995, 1997 Free Software Foundation, Inc.
+   Copyright (C) 1993,94,95,97,99 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -23,14 +23,15 @@
 #include <hurd/fd.h>
 
 error_t
-_hurd_fd_write (struct hurd_fd *fd, const void *buf, size_t *nbytes)
+_hurd_fd_write (struct hurd_fd *fd,
+		const void *buf, size_t *nbytes, off_t offset)
 {
   error_t err;
   mach_msg_type_number_t wrote;
 
   error_t writefd (io_t port)
     {
-      return __io_write (port, buf, *nbytes, -1, &wrote);
+      return __io_write (port, buf, *nbytes, offset, &wrote);
     }
 
   err = HURD_FD_PORT_USE (fd, _hurd_ctty_output (port, ctty, writefd));
