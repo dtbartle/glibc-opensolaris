f8bc3ddac54c68ae9bbcb38c6a8ca7bdf5239dd9
drepper <drepper>
1999-08-04 00:48:08 +0000

    Include errno.h.

--- inet/ether_hton.c
+++ inet/ether_hton.c
@@ -17,6 +17,7 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
+#include <errno.h>
 #include <netinet/ether.h>
 #include <netinet/if_ether.h>
 #include <string.h>
