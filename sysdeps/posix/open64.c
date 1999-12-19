455427399d6e76a631aa3a22e7c18d8b53d41b47
drepper <drepper>
1999-12-19 03:38:24 +0000

    Help gcc generating better code.

--- sysdeps/posix/open64.c
+++ sysdeps/posix/open64.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1995, 1996, 1997, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -26,7 +26,7 @@ __open64 (file, oflag)
      const char *file;
      int oflag;
 {
-  int mode;
+  int mode = 0;
 
   if (oflag & O_CREAT)
     {
@@ -35,8 +35,6 @@ __open64 (file, oflag)
       mode = va_arg (arg, int);
       va_end (arg);
     }
-  else
-    mode = 0;
 
   return __open (file, oflag | O_LARGEFILE, mode);
 }
