b0473714a068d646d3a457341cc48db69b587526
drepper <drepper>
2000-04-14 07:42:10 +0000

    (__fstatfs64): Renamed to fstatfs64. Make old name weak alias.

--- sysdeps/generic/fstatfs64.c
+++ sysdeps/generic/fstatfs64.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1998, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -21,11 +21,12 @@
 
 /* Return information about the filesystem on which FD resides.  */
 int
-fstatfs64 (int fd, struct statfs64 *buf)
+__fstatfs64 (int fd, struct statfs64 *buf)
 {
   __set_errno (ENOSYS);
   return -1;
 }
+weak_alias (__fstatfs64, fstatfs64)
 
 stub_warning (fstatfs64)
 #include <stub-tag.h>
