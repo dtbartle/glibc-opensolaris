eccaaf6cfa458be0f809700a5b2f2a889a9b103c
drepper <drepper>
2000-08-12 04:37:59 +0000

    Use it for getdirentries64 and old_getdirentries64 as well.

--- sysdeps/unix/sysv/linux/getdirentries.c
+++ sysdeps/unix/sysv/linux/getdirentries.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 95, 96, 97, 98, 99 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 95, 96, 97, 98, 99, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -19,20 +19,21 @@
 #include <dirent.h>
 #include <unistd.h>
 
-#ifdef GETDENTS64
-# define getdirentries getdirentries64
-# define __getdents __getdents64
+#ifndef GETDIRENTRIES
+# define GETDIRENTRIES getdirentries
+# define __GETDENTS __getdents
+#else
 # define off_t off64_t
 # define __lseek __lseek64
 #endif
 
 ssize_t
-getdirentries (int fd, char *buf, size_t nbytes, off_t *basep)
+GETDIRENTRIES (int fd, char *buf, size_t nbytes, off_t *basep)
 {
   off_t base = __lseek (fd, (off_t) 0, SEEK_CUR);
   ssize_t result;
 
-  result = __getdents (fd, buf, nbytes);
+  result = __GETDENTS (fd, buf, nbytes);
 
   if (result != -1)
     *basep = base;
