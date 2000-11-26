a5157025e7cb58af92e781a65107ca019063014c
drepper <drepper>
2000-11-26 09:44:12 +0000

    Add prototypes for __old_getrpcbyname_r, __old_getrpcbynumber_r, __old_getrpcent_r.

--- include/rpc/netdb.h
+++ include/rpc/netdb.h
@@ -4,11 +4,21 @@
 extern int __getrpcbyname_r (__const char *__name, struct rpcent *__result_buf,
 			     char *__buffer, size_t __buflen,
 			     struct rpcent **__result);
+extern int __old_getrpcbyname_r (__const char *__name,
+				 struct rpcent *__result_buf,
+				 char *__buffer, size_t __buflen,
+				 struct rpcent **__result);
 
 extern int __getrpcbynumber_r (int __number, struct rpcent *__result_buf,
 			       char *__buffer, size_t __buflen,
 			       struct rpcent **__result);
+extern int __old_getrpcbynumber_r (int __number, struct rpcent *__result_buf,
+				   char *__buffer, size_t __buflen,
+				   struct rpcent **__result);
 
 extern int __getrpcent_r (struct rpcent *__result_buf, char *__buffer,
 			  size_t __buflen, struct rpcent **__result);
+extern int __old_getrpcent_r (struct rpcent *__result_buf, char *__buffer,
+			      size_t __buflen, struct rpcent **__result);
+
 #endif
