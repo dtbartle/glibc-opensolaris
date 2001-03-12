81d8cb41acb650677f3b684cf5ca47ef8141c8ba
drepper <drepper>
2001-03-12 07:15:27 +0000

    Rename mp_dbl to __mp_dbl.

--- sysdeps/ieee754/dbl-64/s_atan.c
+++ sysdeps/ieee754/dbl-64/s_atan.c
@@ -217,7 +217,7 @@ for (i=0; i<M; i++) {
     dbl_mp(x,&mpx,p);          __mpatan(&mpx,&mpy,p);
     dbl_mp(u9[i].d,&mpt1,p);   mul(&mpy,&mpt1,&mperr,p);
     add(&mpy,&mperr,&mpy1,p);  sub(&mpy,&mperr,&mpy2,p);
-    mp_dbl(&mpy1,&y1,p);       mp_dbl(&mpy2,&y2,p);
+    __mp_dbl(&mpy1,&y1,p);       __mp_dbl(&mpy2,&y2,p);
     if (y1==y2)   return y1;
   }
   return y1; /*if unpossible to do exact computing */
