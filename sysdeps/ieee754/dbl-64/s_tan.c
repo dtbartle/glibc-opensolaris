77ce862af14fbb359b5b2f8e45d23fad7635991e
drepper <drepper>
2001-03-12 07:19:16 +0000

    Rename mp_dbl to __mp_dbl.

--- sysdeps/ieee754/dbl-64/s_tan.c
+++ sysdeps/ieee754/dbl-64/s_tan.c
@@ -385,8 +385,8 @@ double tan(double x) {
     /* Second stage */
     /* Reduction by algorithm iv */
     p=10;    n = (mpranred(x,&mpa,p)) & 0x00000001;
-    mp_dbl(&mpa,&a,p);        dbl_mp(a,&mpt1,p);
-    sub(&mpa,&mpt1,&mpt2,p);  mp_dbl(&mpt2,&da,p);
+    __mp_dbl(&mpa,&a,p);        dbl_mp(a,&mpt1,p);
+    sub(&mpa,&mpt1,&mpt2,p);  __mp_dbl(&mpt2,&da,p);
 
     MUL2(a,da,a,da,x2,xx2,t1,t2,t3,t4,t5,t6,t7,t8)
     c1 = x2*(a15.d+x2*(a17.d+x2*(a19.d+x2*(a21.d+x2*(a23.d+x2*(a25.d+
