6ce78326a976ad0f1b00952959b3bbb0d1380872
roland <roland>
2002-09-11 20:11:45 +0000

    2002-09-10  Roland McGrath  <roland@redhat.com>
    	* nscd/Makefile (CPPFLAGS-nscd, CPPFLAGS-nscd_conf, CPPFLAGS-dbg_log)
    	(CPPFLAGS-connections, CPPFLAGS-hstcache): Variables removed.
    	Instead, catch all of $(nscd-modules) via cppflags-iterator.mk.

--- cppflags-iterator.mk
+++ cppflags-iterator.mk
@@ -5,3 +5,14 @@ cpp-src := $(firstword $(cpp-srcs-left))
 cpp-srcs-left := $(filter-out $(cpp-src),$(cpp-srcs-left))
 
 CPPFLAGS-$(cpp-src) = -DNOT_IN_libc -DIS_IN_$(lib)
+
+ifneq ($(lib),rtld)
+ifneq (,$(filter -ftls-model=%,$(CFLAGS-.os)))
+CFLAGS-$(cpp-src:.c=.os) += -ftls-model=global-dynamic
+endif
+ifeq (,$(filter lib%,$(lib)))
+ifneq (,$(filter -ftls-model=%,$(CFLAGS-.o)))
+CFLAGS-$(cpp-src:.c=.o) += -ftls-model=initial-exec
+endif
+endif
+endif
