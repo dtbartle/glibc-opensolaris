1491c3b553f0392c8c6b5619b06cf48637acd319
drepper <drepper>
2003-01-03 00:37:21 +0000

    (__pthread_fork): Protect weak_extern with #ifndef SHARED.

--- linuxthreads/sysdeps/unix/sysv/linux/fork.c
+++ linuxthreads/sysdeps/unix/sysv/linux/fork.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002 Free Software Foundation, Inc.
+/* Copyright (C) 2002, 2003 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Jakub Jelinek <jakub@redhat.com>, 2002.
 
@@ -21,7 +21,9 @@
 #include <fork.h>
 #include <bits/libc-lock.h>
 
+#ifndef SHARED
 weak_extern (__pthread_fork);
+#endif
 
 struct fork_block __fork_block =
 {
