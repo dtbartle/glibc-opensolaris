b558a10de8dbe31d8cbb14872dd0d761607b3c51
drepper <drepper>
2003-02-05 07:06:35 +0000

    (atomic_bit_set): Improve implementation.

--- nptl/atomic.h
+++ nptl/atomic.h
@@ -82,14 +82,14 @@
 #ifndef atomic_bit_set
 # define atomic_bit_set(mem, bit) \
   (void) ({ __typeof (mem) __memp = (mem);				      \
-	    while (1)							      \
-	      {								      \
-		__typeof (*mem) __oldval = *__memp;			      \
+	    __typeof (*mem) __mask = (1 << (bit));			      \
+	    __typeof (*mem) __oldval;					      \
 									      \
-		if (atomic_compare_and_exchange_acq (__memp,		      \
-						     __oldval | 1 << bit,     \
-						     __oldval) == 0)	      \
-		  break;						      \
+	    do								      \
+	      __oldval = *__memp;					      \
+	     while (atomic_compare_and_exchange_acq (__memp,		      \
+						     __oldval | __mask,	      \
+						     __oldval));	      \
 	      }})
 #endif
 
