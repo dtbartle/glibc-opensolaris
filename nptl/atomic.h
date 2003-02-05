55498498aac561ad833c533b8c6a338f72c5af68
drepper <drepper>
2003-02-05 07:09:40 +0000

    (atomic_compare_and_exchange_acq): Use __arch_compare_and_exchange_32_acq in return value definition.  It always exists.

--- nptl/atomic.h
+++ nptl/atomic.h
@@ -27,7 +27,7 @@
 
 #ifndef atomic_compare_and_exchange_acq
 # define atomic_compare_and_exchange_acq(mem, newval, oldval) \
-  ({ __typeof (__arch_compare_and_exchange_8_acq (mem, newval, oldval))	      \
+  ({ __typeof (__arch_compare_and_exchange_32_acq (mem, newval, oldval))      \
 	 __result;							      \
      if (sizeof (*mem) == 1)						      \
        __result = __arch_compare_and_exchange_8_acq (mem, newval, oldval);    \
