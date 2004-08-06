d84dace427132a290230c7abe22b2673ca7a4c99
drepper <drepper>
2004-08-06 07:42:01 +0000

    (setsourcefilter): Correctly detect when alloca can be used.

--- sysdeps/unix/sysv/linux/setsourcefilter.c
+++ sysdeps/unix/sysv/linux/setsourcefilter.c
@@ -34,17 +34,17 @@ setsourcefilter (int s, uint32_t interface, struct sockaddr *group,
   /* We have to create an struct ip_msfilter object which we can pass
      to the kernel.  */
   size_t needed = GROUP_FILTER_SIZE (numsrc);
-  int use_malloc = __libc_use_alloca (needed);
+  int use_alloca = __libc_use_alloca (needed);
 
   struct group_filter *gf;
-  if (use_malloc)
+  if (use_alloca)
+    gf = (struct group_filter *) alloca (needed);
+  else
     {
       gf = (struct group_filter *) malloc (needed);
       if (gf == NULL)
 	return -1;
     }
-  else
-    gf = (struct group_filter *) alloca (needed);
 
   gf->gf_interface = interface;
   memcpy (&gf->gf_group, group, grouplen);
@@ -54,7 +54,7 @@ setsourcefilter (int s, uint32_t interface, struct sockaddr *group,
 
   int result = __setsockopt (s, SOL_IP, MCAST_MSFILTER, gf, needed);
 
-  if (use_malloc)
+  if (! use_alloca)
     {
       int save_errno = errno;
       free (gf);
