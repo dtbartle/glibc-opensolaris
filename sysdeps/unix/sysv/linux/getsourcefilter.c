5bcf1e326e7a2af51b5b85c8f4524f4c919f2185
drepper <drepper>
2004-08-06 09:00:09 +0000

    (__get_sol): New function. (getsourcefilter): Use __get_sol to determine correct socket level value.

--- sysdeps/unix/sysv/linux/getsourcefilter.c
+++ sysdeps/unix/sysv/linux/getsourcefilter.c
@@ -19,12 +19,72 @@
    02111-1307 USA.  */
 
 #include <alloca.h>
+#include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netatalk/at.h>
+#include <netax25/ax25.h>
 #include <netinet/in.h>
+#include <netipx/ipx.h>
+#include <netpacket/packet.h>
+#include <netrose/rose.h>
 #include <sys/param.h>
 #include <sys/socket.h>
+
+
+static const struct
+{
+  int sol;
+  int af;
+  socklen_t size;
+}  sol_map[] =
+  {
+    /* Sort the array according to importance of the protocols.  Add
+       more protocols when they become available.  */
+    { SOL_IP, AF_INET, sizeof (struct sockaddr_in) },
+    { SOL_IPV6, AF_INET6, sizeof (struct sockaddr_in6) },
+    { SOL_AX25, AF_AX25, sizeof (struct sockaddr_ax25) },
+    { SOL_IPX, AF_IPX, sizeof (struct sockaddr_ipx) },
+    { SOL_ATALK, AF_APPLETALK, sizeof (struct sockaddr_at) },
+    { SOL_ROSE, AF_ROSE, sizeof (struct sockaddr_rose) },
+    { SOL_PACKET, AF_PACKET, sizeof (struct sockaddr_ll) }
+  };
+#define NSOL_MAP (sizeof (sol_map) / sizeof (sol_map[0]))
+
+
+/* Try to determine the socket level value.  Ideally both side and
+   family are set.  But sometimes only the size is correct and the
+   family value might be bogus.  Loop over the array entries and look
+   for a perfect match or the first match based on size.  */
+int
+__get_sol (int af, socklen_t len)
+{
+  int first_size_sol = -1;
+
+  for (size_t cnt = 0; cnt < NSOL_MAP; ++cnt)
+    {
+      /* Just a test so that we make sure the special value used to
+	 signal the we have so far no socket level value is OK.  */
+      assert (sol_map[cnt].sol != -1);
+
+      if (len == sol_map[cnt].size)
+	{
+	  /* The size matches, which is a requirement.  If the family
+	     matches, too, we have a winner.  Otherwise we remember the
+	     socket level value for this protocol if it is the first
+	     match.  */
+	  if (af == sol_map[cnt].af)
+	    /* Bingo!  */
+	    return sol_map[cnt].sol;
+
+	  if (first_size_sol == -1)
+	    first_size_sol = sol_map[cnt].sol;
+      }
+    }
+
+  return first_size_sol;
+}
 
 
 int
@@ -50,6 +110,14 @@ getsourcefilter (int s, uint32_t interface, struct sockaddr *group,
   gf->gf_interface = interface;
   memcpy (&gf->gf_group, group, grouplen);
   gf->gf_numsrc = *numsrc;
+
+  /* We need to provide the appropriate socket level value.  */
+  int sol = __get_sol (group->sa_family, grouplen);
+  if (sol == -1)
+    {
+      __set_errno (EINVAL);
+      return -1;
+    }
 
   int result = __getsockopt (s, SOL_IP, MCAST_MSFILTER, gf, &needed);
 
