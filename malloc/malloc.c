19a21500eb148be0f60e3bdf5c67f06e430f64e4
drepper <drepper>
2004-11-19 21:22:47 +0000

    (_int_free): Add a few more cheap tests for corruption.

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -4233,6 +4233,15 @@ _int_free(mstate av, Void_t* mem)
 #endif
       ) {
 
+    if (__builtin_expect (chunksize (chunk_at_offset (p, size)) <= 2 * SIZE_SZ,
+			  0)
+	|| __builtin_expect (chunksize (chunk_at_offset (p, size))
+			     >= av->system_mem, 0))
+      {
+	errstr = "invalid next size (fast)";
+	goto errout;
+      }
+
     set_fastchunks(av);
     fb = &(av->fastbins[fastbin_index(size)]);
     /* Another simple check: make sure the top of the bin is not the
@@ -4276,7 +4285,12 @@ _int_free(mstate av, Void_t* mem)
       }
 
     nextsize = chunksize(nextchunk);
-    assert(nextsize > 0);
+    if (__builtin_expect (nextsize <= 2 * SIZE_SZ, 0)
+	|| __builtin_expect (nextsize >= av->system_mem, 0))
+      {
+	errstr = "invalid next size (normal)";
+	goto errout;
+      }
 
     /* consolidate backward */
     if (!prev_inuse(p)) {
