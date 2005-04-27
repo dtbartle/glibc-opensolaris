5593375a6d59edf85ac846a5904602328d69bd98
roland <roland>
2005-04-27 08:02:21 +0000

    2005-04-27  Roland McGrath  <roland@redhat.com>
    	* sysdeps/pthread/getcpuclockid.c (pthread_getcpuclockid)
    	[__NR_clock_getres]: Use kernel-supplied CPU clocks if available.

--- linuxthreads/sysdeps/pthread/getcpuclockid.c
+++ linuxthreads/sysdeps/pthread/getcpuclockid.c
@@ -1,4 +1,5 @@
-/* Copyright (C) 2000, 2001, 2004 Free Software Foundation, Inc.
+/* pthread_getcpuclockid -- Get POSIX clockid_t for a pthread_t.  Linux version
+   Copyright (C) 2000, 2001, 2004 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -21,10 +22,76 @@
 #include <sys/time.h>
 #include <time.h>
 #include <internals.h>
+#include "kernel-features.h"
+#include "posix-cpu-timers.h"
+
+
+#if !(__ASSUME_POSIX_CPU_TIMERS > 0)
+int __libc_missing_posix_cpu_timers attribute_hidden;
+#endif
+#if !(__ASSUME_POSIX_TIMERS > 0)
+int __libc_missing_posix_timers attribute_hidden;
+#endif
 
 int
 pthread_getcpuclockid (pthread_t thread_id, clockid_t *clock_id)
 {
+#ifdef __NR_clock_getres
+  pthread_handle handle = thread_handle(thread_id);
+  int pid;
+
+  __pthread_lock (&handle->h_lock, NULL);
+  if (nonexisting_handle (handle, thread_id))
+    {
+      __pthread_unlock (&handle->h_lock);
+      return ESRCH;
+    }
+  pid = handle->h_descr->p_pid;
+  __pthread_unlock (&handle->h_lock);
+
+  /* The clockid_t value is a simple computation from the PID.
+     But we do a clock_getres call to validate it if we aren't
+     yet sure we have the kernel support.  */
+
+  const clockid_t pidclock = MAKE_PROCESS_CPUCLOCK (pid, CPUCLOCK_SCHED);
+
+# if !(__ASSUME_POSIX_CPU_TIMERS > 0)
+#  if !(__ASSUME_POSIX_TIMERS > 0)
+  if (__libc_missing_posix_timers && !__libc_missing_posix_cpu_timers)
+    __libc_missing_cpu_posix_timers = 1;
+#  endif
+  if (!__libc_missing_posix_cpu_timers)
+    {
+      INTERNAL_SYSCALL_DECL (err);
+      int r = INTERNAL_SYSCALL (clock_getres, err, 2, tidclock, NULL);
+      if (!INTERNAL_SYSCALL_ERROR_P (r, err))
+# endif
+	{
+	  *clock_id = pidclock;
+	  return 0;
+	}
+
+# if !(__ASSUME_POSIX_CPU_TIMERS > 0)
+#  if !(__ASSUME_POSIX_TIMERS > 0)
+      if (INTERNAL_SYSCALL_ERRNO (r, err) == ENOSYS)
+	{
+	  /* The kernel doesn't support these calls at all.  */
+	  __libc_missing_posix_timers = 1;
+	  __libc_missing_posix_cpu_timers = 1;
+	}
+      else
+#  endif
+	if (INTERNAL_SYSCALL_ERRNO (r, err) == EINVAL)
+	  {
+	    /* The kernel doesn't support these clocks at all.  */
+	    __libc_missing_posix_cpu_timers = 1;
+	  }
+      else
+	return INTERNAL_SYSCALL_ERRNO (r, err);
+    }
+# endif
+#endif
+
 #ifdef CLOCK_THREAD_CPUTIME_ID
   /* We need to store the thread ID in the CLOCKID variable together
      with a number identifying the clock.  We reserve the low 3 bits
