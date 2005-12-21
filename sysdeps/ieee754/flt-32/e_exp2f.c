ace8d15f336bdad650db3da1e9cd8da50e7c80ee
drepper <drepper>
2005-12-21 18:13:46 +0000

    Use .rodata.cst4 instead of .rodata.

--- sysdeps/ieee754/flt-32/e_exp2f.c
+++ sysdeps/ieee754/flt-32/e_exp2f.c
@@ -38,9 +38,9 @@
 
 #include "t_exp2f.h"
 
-static const volatile float TWOM100 __attribute__ ((section (".rodata")))
+static const volatile float TWOM100 __attribute__ ((section (".rodata.cst4")))
   = 7.88860905e-31;
-static const volatile float TWO127 __attribute__ ((section (".rodata")))
+static const volatile float TWO127 __attribute__ ((section (".rodata.cst4")))
   = 1.7014118346e+38;
 
 float
