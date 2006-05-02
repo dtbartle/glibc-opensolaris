a58789bdd7bd19449f0feaeeddd4ec71840f58f7
drepper <drepper>
2006-05-02 00:45:11 +0000

    (execute_cfa_program): Fix typo.

--- sysdeps/generic/unwind-dw2.c
+++ sysdeps/generic/unwind-dw2.c
@@ -839,7 +839,7 @@ execute_cfa_program (const unsigned char *insn_ptr,
 	  {
 	    struct frame_state_reg_info *old_rs = fs->regs.prev;
 #ifdef _LIBC
-	    if (rs == NULL)
+	    if (old_rs == NULL)
 	      __libc_fatal ("invalid DWARF unwind data");
 	    else
 #endif
