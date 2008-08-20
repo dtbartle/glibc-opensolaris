0f1515ac4e34dc5a1578474a2641a16662e03d26
David Bartley <dtbartle@csclub.uwaterloo.ca>
2008-08-19 23:16:32 -0400

    opensolaris -> kopensolaris in sigprocmask.c

--- nptl/sysdeps/unix/sysv/solaris2/kopensolaris-gnu/sigprocmask.c
+++ nptl/sysdeps/unix/sysv/solaris2/kopensolaris-gnu/sigprocmask.c
@@ -1,6 +1,6 @@
 #ifndef __SIGPROCMASK_C
 #define __SIGPROCMASK_C
-#include <sysdeps/unix/sysv/solaris2/opensolaris-gnu/sigprocmask.c>
+#include <sysdeps/unix/sysv/solaris2/kopensolaris-gnu/sigprocmask.c>
 #else
-#include_next <sysdeps/unix/sysv/solaris2/opensolaris-gnu/sigprocmask.c>
+#include_next <sysdeps/unix/sysv/solaris2/kopensolaris-gnu/sigprocmask.c>
 #endif /* __SIGPROCMASK_C */
