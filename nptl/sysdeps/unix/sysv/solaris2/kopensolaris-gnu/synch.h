7ff6a375ad719a4bf74c23f4f7010d29a4997500
David Bartley <dtbartle@maltodextrin.csclub.uwaterloo.ca>
2008-09-13 16:43:01 +0000

    Implement _mutex_held

--- nptl/sysdeps/unix/sysv/solaris2/kopensolaris-gnu/synch.h
+++ nptl/sysdeps/unix/sysv/solaris2/kopensolaris-gnu/synch.h
@@ -47,42 +47,46 @@ int _lwp_sema_post (lwp_sema_t *);
 
 /* condition variables */
 
-int cond_init(cond_t *, int, void *);
-int cond_destroy(cond_t *);
-int cond_wait(cond_t *, mutex_t *);
-int cond_timedwait(cond_t *, mutex_t *, struct timespec *);
-int cond_reltimedwait(cond_t *, mutex_t *, struct timespec *);
-int cond_signal(cond_t *);
-int cond_broadcast(cond_t *);
+int cond_init (cond_t *, int, void *);
+int cond_destroy (cond_t *);
+int cond_wait (cond_t *, mutex_t *);
+int cond_timedwait (cond_t *, mutex_t *, struct timespec *);
+int cond_reltimedwait (cond_t *, mutex_t *, struct timespec *);
+int cond_signal (cond_t *);
+int cond_broadcast (cond_t *);
 
 /* mutexes */
 
-int mutex_init(mutex_t *, int, void *);
-int mutex_destroy(mutex_t *);
-int mutex_consistent(mutex_t *);
-int mutex_lock(mutex_t *);
-int mutex_trylock(mutex_t *);
-int mutex_unlock(mutex_t *);
+extern int mutex_init (mutex_t *, int, void *);
+extern int mutex_destroy (mutex_t *);
+extern int mutex_consistent (mutex_t *);
+extern int mutex_lock (mutex_t *);
+extern int mutex_trylock (mutex_t *);
+extern int mutex_unlock (mutex_t *);
+extern int _mutex_held (mutex_t *);
 
 /* reader/writer locks */
 
-int rwlock_init(rwlock_t *, int, void *);
-int rwlock_destroy(rwlock_t *);
-int rw_rdlock(rwlock_t *);
-int rw_wrlock(rwlock_t *);
-int rw_unlock(rwlock_t *);
-int rw_tryrdlock(rwlock_t *);
-int rw_trywrlock(rwlock_t *);
+extern int rwlock_init (rwlock_t *, int, void *);
+extern int rwlock_destroy (rwlock_t *);
+extern int rw_rdlock (rwlock_t *);
+extern int rw_wrlock (rwlock_t *);
+extern int rw_unlock (rwlock_t *);
+extern int rw_tryrdlock (rwlock_t *);
+extern int rw_trywrlock (rwlock_t *);
+extern int _rw_read_held (rwlock_t *);
+extern int _rw_write_held (rwlock_t *);
 
 /* semaphores */
 
-int sema_init(sema_t *, unsigned int, int, void *);
-int sema_destroy(sema_t *);
-int sema_wait(sema_t *);
-int sema_timedwait(sema_t *, struct timespec *);
-int sema_reltimedwait(sema_t *, const struct timespec *);
-int sema_post(sema_t *);
-int sema_trywait(sema_t *);
+extern int sema_init (sema_t *, unsigned int, int, void *);
+extern int sema_destroy (sema_t *);
+extern int sema_wait (sema_t *);
+extern int sema_timedwait (sema_t *, struct timespec *);
+extern int sema_reltimedwait (sema_t *, const struct timespec *);
+extern int sema_post (sema_t *);
+extern int sema_trywait (sema_t *);
+extern int _sema_held (sema_t *);
 
 __END_DECLS
 
