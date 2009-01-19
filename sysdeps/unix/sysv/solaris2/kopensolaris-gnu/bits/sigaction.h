ca72065a17e78923a195ee7cf90a887de0eb0365
rmh <rmh@thorin.(none)>
2009-01-19 15:12:26 +0100

    Add some compatibility aliases (copied from generic bits/sigaction.h).

--- sysdeps/unix/sysv/solaris2/kopensolaris-gnu/bits/sigaction.h
+++ sysdeps/unix/sysv/solaris2/kopensolaris-gnu/bits/sigaction.h
@@ -1,5 +1,5 @@
 /* The definitions for OpenSolaris's sigaction.
-   Copyright (C) 1993-1999, 2000, 2008 Free Software Foundation, Inc.
+   Copyright (C) 1993-1999, 2000, 2008, 2009 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -70,6 +70,15 @@ struct sigaction32
 #define SA_NODEFER		0x00000010	/* Don't automatically block the signal when
                                         its handler is being executed.  */
 #define SA_RESETHAND	0x00000002
+#endif
+
+#ifdef __USE_MISC
+# define SA_INTERRUPT	0	/* Historical no-op ("not SA_RESTART").  */
+
+/* Some aliases for the SA_ constants.  */
+# define SA_NOMASK    SA_NODEFER
+# define SA_ONESHOT   SA_RESETHAND
+# define SA_STACK     SA_ONSTACK
 #endif
 
 /* Values for the HOW argument to `sigprocmask'.  */
