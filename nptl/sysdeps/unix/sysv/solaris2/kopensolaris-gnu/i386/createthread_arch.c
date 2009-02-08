3a6fcd4967c6c23c79aea08e6335a39a43cdeeb0
David Bartley <dtbartle@glibc.(none)>
2009-02-08 07:49:10 +0000

    Remove sys/segments.h dependency

--- nptl/sysdeps/unix/sysv/solaris2/kopensolaris-gnu/i386/createthread_arch.c
+++ nptl/sysdeps/unix/sysv/solaris2/kopensolaris-gnu/i386/createthread_arch.c
@@ -17,7 +17,7 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
-#include <sys/segments.h>
+#define LWPGS_SEL	0x01C3
 
 static int
 create_thread_arch (ucontext_t *ctx, struct pthread *pd,
